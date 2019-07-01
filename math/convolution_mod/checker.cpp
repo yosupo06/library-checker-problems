beb18d56edc7b4969e4914884e93626010b211bb
Kohei Morita <yosupo06@gmail.com>
2019-07-02 00:03:11 +0900

    fix checker.cpp

--- math/convolution_mod/checker.cpp
+++ math/convolution_mod/checker.cpp
@@ -1,32 +1,32 @@
 // https://github.com/MikeMirzayanov/testlib/blob/master/checkers/wcmp.cpp
-//
-// The MIT License(MIT)
-//
-// Copyright(c) 2015 Mike Mirzayanov
-//
-// Permission is hereby granted,
-// free of charge, to any person obtaining a copy of this software and associated documentation files(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and / or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions :
-//
-// The above copyright notice and this permission notice shall be included in all copies
-// or
-// substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS",
-// WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-// DAMAGES OR OTHER
-// LIABILITY,
-// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE. 
 
+// The MIT License (MIT)
+
+// Copyright (c) 2015 Mike Mirzayanov
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #include "testlib.h"
 
-    using namespace std;
+using namespace std;
 
-int main(int argc, char *argv[])
+int main(int argc, char * argv[])
 {
     setName("compare sequences of tokens");
     registerTestlibCmd(argc, argv);
@@ -34,13 +34,13 @@ int main(int argc, char *argv[])
     int n = 0;
     string j, p;
 
-    while (!ans.seekEof() && !ouf.seekEof())
+    while (!ans.seekEof() && !ouf.seekEof()) 
     {
         n++;
 
         ans.readWordTo(j);
         ouf.readWordTo(p);
-
+        
         if (j != p)
             quitf(_wa, "%d%s words differ - expected: '%s', found: '%s'", n, englishEnding(n).c_str(), compress(j).c_str(), compress(p).c_str());
     }

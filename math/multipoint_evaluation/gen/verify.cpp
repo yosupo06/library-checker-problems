8aa8960ec09911a543e2f6ec2cf40959cc70481a
Kohei Morita <yosupo06@gmail.com>
2019-09-04 02:06:44 +0900

    add multipoint_evaluation

---
+++ math/multipoint_evaluation/gen/verify.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "testlib.h"
+
+int main() {
+    registerValidation();
+    int n = inf.readInt(1, 1 << 17);
+    inf.readSpace();
+    int m = inf.readInt(1, 1 << 17);
+    inf.readEoln();
+    for (int i = 0; i < n; i++) {
+        inf.readInt((i != n - 1) ? 0 : 1, 998244353 - 1);
+        if (i != n - 1) inf.readSpace();
+    }
+    inf.readEoln();
+    for (int i = 0; i < m; i++) {
+        inf.readInt(0, 998244353 - 1);
+        if (i != m - 1) inf.readSpace();
+    }
+    inf.readEoln();
+
+    inf.readEof();
+    return 0;
+}

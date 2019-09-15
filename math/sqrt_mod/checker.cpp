47df3cede1989f47c4ae395c6a417486a940f8d3
yosupo06 <yosupo06@gmail.com>
2019-09-16 02:55:12 +0900

    fix issue61 (#62)

--- math/sqrt_mod/checker.cpp
+++ math/sqrt_mod/checker.cpp
@@ -16,27 +16,6 @@ T pow_mod(T x, U n, T md) {
     return r;
 }
 
-bool is_prime(ll n) {
-    if (n <= 1) return false;
-    if (n == 2) return true;
-    if (n % 2 == 0) return false;
-    ll d = n - 1;
-    while (d % 2 == 0) d /= 2;
-    for (ll a : {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}) {
-        if (n <= a) break;
-        ll t = d;
-        ll y = pow_mod<__int128_t>(a, t, n);  // over
-        while (t != n - 1 && y != 1 && y != n - 1) {
-            y = __int128_t(y) * y % n;  // flow
-            t <<= 1;
-        }
-        if (y != n - 1 && t % 2 == 0) {
-            return false;
-        }
-    }
-    return true;
-}
-
 int main(int argc, char *argv[]) {
     registerTestlibCmd(argc, argv);
 

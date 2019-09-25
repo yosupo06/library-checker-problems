5e15cff634c8ce2d77b395cca3bac58103d0e3bc
Kohei Morita <yosupo06@gmail.com>
2019-09-26 03:22:20 +0900

    add system_of_linear_equations

---
+++ math/system_of_linear_equations/gen/random.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include "random.h"
+
+using namespace std;
+
+int main(int, char* argv[]) {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    int n = gen.uniform(1, 500);
+    int m = gen.uniform(1, 500);
+
+    vector<vector<int>> a(n, vector<int>(m));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            a[i][j] = gen.uniform(0, 998244353 - 1);
+        }
+    }
+
+    vector<int> b(n);
+    for (int i = 0; i < n; i++) {
+        b[i] = gen.uniform(0, 998244353 - 1);
+    }
+
+    cout << n << " " << m << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cout << a[i][j];
+            if (j + 1 != m) cout << " ";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < n; i++) {
+        cout << b[i];
+        if (i + 1 != n) cout << " ";
+    }
+    cout << endl;
+
+    return 0;
+}

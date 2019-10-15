6b8083b9bcfd558a23a97d7541addd71a0dcc41a
Kohei Morita <yosupo06@gmail.com>
2019-10-15 14:09:20 +0000

    fix discrete_logarithm

--- math/discrete_logarithm_mod/checker.cpp
+++ math/discrete_logarithm_mod/checker.cpp
@@ -1,62 +1,58 @@
-// https://github.com/MikeMirzayanov/testlib/blob/master/checkers/wcmp.cpp
-
-// The MIT License (MIT)
-
-// Copyright (c) 2015 Mike Mirzayanov
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
+#include <algorithm>
+#include <iostream>
+#include <cassert>
 #include "testlib.h"
 
 using namespace std;
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+template <class T> using V = vector<T>;
 
-int main(int argc, char * argv[])
-{
-    setName("compare sequences of tokens");
+ll pow_mod(ll x, ll n, ll md) {
+    ll r = 1 % md;
+    x %= md;
+    while (n) {
+        if (n & 1) r = (r * x) % md;
+        x = (x * x) % md;
+        n >>= 1;
+    }
+    return r;
+}
+
+ll read_ans(ll x, ll y, ll m, InStream& stream) {
+    ll k = stream.readLong(-1, m);
+    if (k != -1 && pow_mod(x, k, m) != y) stream.quit(_wa, "x^k != y");
+    return k;
+}
+
+int main(int argc, char* argv[]) {
     registerTestlibCmd(argc, argv);
 
-    int n = 0;
-    string j, p;
+    int t = inf.readInt();
+    for (int i = 0; i < t; i++) {
+        ll x = inf.readLong();
+        ll y = inf.readLong();
+        ll m = inf.readLong();
 
-    while (!ans.seekEof() && !ouf.seekEof()) 
-    {
-        n++;
+        ll expect = read_ans(x, y, m, ans);
+        ll actual = read_ans(x, y, m, ouf);
 
-        ans.readWordTo(j);
-        ouf.readWordTo(p);
-        
-        if (j != p)
-            quitf(_wa, "%d%s words differ - expected: '%s', found: '%s'", n, englishEnding(n).c_str(), compress(j).c_str(), compress(p).c_str());
-    }
+        // ok
+        if (expect == actual) continue;
 
-    if (ans.seekEof() && ouf.seekEof())
-    {
-        if (n == 1)
-            quitf(_ok, "\"%s\"", compress(j).c_str());
-        else
-            quitf(_ok, "%d tokens", n);
+        if (expect != -1 && actual == -1) {
+            quitf(_wa, "Participate can't find solution");
+        }
+        if (expect == -1 && actual != -1) {
+            quitf(_fail, "Judge can't find solution, wow!");
+        }
+        if (actual < expect) {
+            quitf(_fail, "Participate find more better solution!");
+        }
+        if (actual > expect) {
+            quitf(_wa, "Participate can't find best solution");
+        }
     }
-    else
-    {
-        if (ans.seekEof())
-            quitf(_wa, "Participant output contains extra tokens");
-        else
-            quitf(_wa, "Unexpected EOF in the participants output");
-    }
+    quitf(_ok, "OK");
 }

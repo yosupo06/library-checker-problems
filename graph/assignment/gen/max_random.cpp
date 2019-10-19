2c1b751c98a743142364f2423af4785a94f88865
Kohei Morita <yosupo06@gmail.com>
2019-10-19 10:18:13 +0000

    add max_random for assignment

---
+++ graph/assignment/gen/max_random.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "random.h"
+
+using namespace std;
+
+int main(int, char* argv[]) {
+    cin.tie(nullptr);
+    ios::sync_with_stdio(false);
+
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    int n = 500;
+    vector<vector<int>> a(n, vector<int>(n));
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            a[i][j] = gen.uniform(-1'000'000'000, 1'000'000'000);
+        }
+    }
+
+
+    cout << n << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << a[i][j];
+            if (j != n - 1) cout << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}

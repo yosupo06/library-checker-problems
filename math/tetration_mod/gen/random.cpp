470de8c770e21183fdcaa1fb86df9fc797b2ca9d
beet-aizu <aki.bdash@gmail.com>
2019-12-17 19:37:45 +0900

    fix

--- math/tetration_mod/gen/random.cpp
+++ math/tetration_mod/gen/random.cpp
@@ -14,10 +14,10 @@ int main(int, char* argv[]) {
     int t = gen.uniform(1, 100);
     cout << t << "\n";
     for (int i = 0; i < t; i++) {
-        ll m = gen.uniform(1LL, 1'000'000'000);
-        ll b = gen.uniform(0LL, 1'000'000'000);
+        ll m = gen.uniform(1LL, 1'000'000'000LL);
         ll a = gen.uniform(0LL, m - 1);
-        cout << a << " " << b << " " << p << endl;
+        ll b = gen.uniform(0LL, 1'000'000'000LL);
+        cout << a << " " << b << " " << m << endl;
     }
     return 0;
 }

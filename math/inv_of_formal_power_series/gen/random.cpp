95b58ee829ee84374bf62f0d5213df241ecd7cf0
Kohei Morita <yosupo06@gmail.com>
2019-12-28 17:06:30 +0900

    iostream -> cstdio: inv_of_formal_power_series

--- math/inv_of_formal_power_series/gen/random.cpp
+++ math/inv_of_formal_power_series/gen/random.cpp
@@ -1,23 +1,20 @@
 #include "random.h"
-#include <iostream>
+#include <cstdio>
 
 using namespace std;
 using ll = long long;
 
 int main(int, char* argv[]) {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-
     long long seed = atoll(argv[1]);
     auto gen = Random(seed);
 
     int n = gen.uniform(1, 500'000);
 
-    cout << n << endl;
+    printf("%d\n", n);
     for (int i = 0; i < n; i++) {
-        cout << gen.uniform((i == 0) ? 1 : 0, 998244353);
-        if (i != n - 1) cout << " ";
+        printf("%d", gen.uniform((i == 0) ? 1 : 0, 998244353));
+        if (i != n - 1) printf(" ");
     }
-    cout << endl;
+    printf("\n");
     return 0;
 }

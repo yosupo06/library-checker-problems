d3af93b064f8b6550825ab7e62b78b60a161d448
Kohei Morita <yosupo06@gmail.com>
2019-12-28 17:01:51 +0900

    iostream -> cstdio: exp_of_formal_power_series

--- math/exp_of_formal_power_series/gen/max_random.cpp
+++ math/exp_of_formal_power_series/gen/max_random.cpp
@@ -1,23 +1,20 @@
-#include <iostream>
+#include <cstdio>
 #include "random.h"
 
 using namespace std;
 using ll = long long;
 
 int main(int, char* argv[]) {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-
     long long seed = atoll(argv[1]);
     auto gen = Random(seed);
 
     int n = 500'000;
-    
-    cout << n << endl;
-    cout << 0;
+    printf("%d\n", n);
+
+    printf("0");
     for (int i = 1; i < n; i++) {
-        cout << " " << gen.uniform(0, 998244353);
+        printf(" %d", gen.uniform(0, 998244353));
     }
-    cout << endl;
+    printf("\n");
     return 0;
 }

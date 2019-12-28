c11269cab8f6e95487144e4ee9c5400a7beac957
niuez <niuez.graph@gmail.com>
2019-12-28 19:35:12 +0900

    [fix dynamic_tree_vertex_add_path_sum] using printf/scanf for fast io

--- datastructure/dynamic_tree_vertex_add_path_sum/gen/max_random.cpp
+++ datastructure/dynamic_tree_vertex_add_path_sum/gen/max_random.cpp
@@ -11,20 +11,17 @@ const int A_AND_X_MIN = 0;
 const int A_AND_X_MAX = 1'000'000'000;
 
 int main(int, char* argv[]) {
-    cin.tie(nullptr);
-    ios::sync_with_stdio(false);
-
     long long seed = atoll(argv[1]);
     auto gen = Random(seed);
 
     int n = N_AND_Q_MAX;
     int q = N_AND_Q_MAX;
-    cout << n << " " << q << "\n";
+    printf("%d %d\n", n, q);
     for (int i = 0; i < n; i++) {
-        cout << gen.uniform(A_AND_X_MIN, A_AND_X_MAX);
-        if (i != n - 1) cout << " ";
+        printf("%d", gen.uniform(A_AND_X_MIN, A_AND_X_MAX));
+        if (i != n - 1) printf(" ");
     }
-    cout << "\n";
+    printf("\n");
 
     vector<int> u(n - 1), v(n - 1);
     for (int i = 0; i < n - 1; i++) {
@@ -43,13 +40,13 @@ int main(int, char* argv[]) {
     dynamic_tree tree(n);
 
     for (int i = 0; i < n - 1; i++) {
-        cout << u[i] << " " << v[i] << "\n";
+        printf("%d %d\n", u[i], v[i]);
         tree.link(u[i], v[i]);
     }
 
     for (int i = 0; i < q; i++) {
         int t = gen.uniform(0, 2);
-        cout << t << " ";
+        printf("%d ", t);
         if(t == 0) {
           int eidx = gen.uniform(0, n - 2);
           auto p = tree.cut(eidx);
@@ -57,16 +54,16 @@ int main(int, char* argv[]) {
           int v = p.second;
           int w = tree.random_vertex(gen, u);
           tree.link(v, w);
-          cout << u << " " << v << " " << w << "\n";
+          printf("%d %d %d\n", u, v, w);
         }
         else if (t == 1) {
             int p = gen.uniform(0, n - 1);
             int x = gen.uniform(A_AND_X_MIN, A_AND_X_MAX);
-            cout << p << " " << x << "\n";
+            printf("%d %d\n", p, x);
         } else {
             auto u = gen.uniform(0, n - 1);
             auto v = gen.uniform(0, n - 1);
-            cout << u << " " << v << "\n";
+            printf("%d %d\n", u, v);
         }
     }
     return 0;
--- datastructure/dynamic_tree_vertex_add_path_sum/sol/naive.cpp
+++ datastructure/dynamic_tree_vertex_add_path_sum/sol/naive.cpp
@@ -21,25 +21,25 @@ i64 dfs(i64 u, i64 f, i64 v, i64 ans) {
 }
 
 int main() {
-  cin >> N >> Q;
+  scanf("%d%d", &N, &Q);
   A.resize(N);
   G.resize(N);
   for(int i = 0;i < N;i++) {
-    cin >> A[i];
+    scanf("%lld", &A[i]);
   }
   for(int i = 0;i < N - 1;i++) {
     i64 u, v;
-    cin >> u >> v;
+    scanf("%lld%lld", &u, &v);
     G[u].insert(v);
     G[v].insert(u);
   }
 
   for(int i = 0; i < Q;i++) {
     i64 q, a, b;
-    cin >> q >> a >> b;
+    scanf("%lld%lld%lld", &q, &a, &b);
     if(q == 0) {
       i64 c;
-      cin >> c;
+      scanf("%lld", &c);
       G[a].erase(b);
       G[b].erase(a);
       G[b].insert(c);

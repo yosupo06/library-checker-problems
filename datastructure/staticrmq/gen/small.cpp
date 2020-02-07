14deaa964004bd1012e6c87ad423e94bc424ed7a
Kohei Morita <yosupo06@gmail.com>
2020-02-07 23:43:21 +0900

    fix #259

---
+++ datastructure/staticrmq/gen/small.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "random.h"
+
+using namespace std;
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    int n = seed % 10 + 1;
+    int q = 1000;
+    printf("%d %d\n", n, q);
+    for (int i = 0; i < n; i++) {
+        printf("%d", gen.uniform(0, 1'000'000'000));
+        if (i != n - 1) printf(" ");
+    }
+    printf("\n");
+    for (int i = 0; i < q; i++) {
+        auto p = gen.uniform_pair(0, n);
+        printf("%d %d\n", p.first, p.second);
+    }
+    return 0;
+}

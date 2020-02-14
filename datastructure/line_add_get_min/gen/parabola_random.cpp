b6de3d92763d7daf1a8f2ef0d5557d5ad88904ce
sansen0796 <ymkw.snsn@gmail.com>
2020-02-15 04:12:10 +0900

    parabola random

---
+++ datastructure/line_add_get_min/gen/parabola_random.cpp
@@ -0,0 +1,34 @@
+#include "random.h"
+#include "../params.h"
+#include <iostream>
+
+using namespace std;
+using ll = long long;
+
+int main(int, char* argv[]) {
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+    int n = N_AND_Q_MAX;
+    int q = N_AND_Q_MAX;
+    printf("%d %d\n", n, q);
+    for (int i = 0; i < n; i++) {
+        ll a = gen.uniform(-A_ABS_MAX, A_ABS_MAX);
+        ll b = a * a / 2;
+        printf("%lld %lld\n", a, b);
+    }
+    ll dec = 0;
+    for (int i = 0; i < q; i++) {
+        int t = gen.uniform(0, 1);
+        printf("%d ", t);
+        if (t == 0) {
+            int a = gen.uniform(-A_ABS_MAX, A_ABS_MAX);
+            dec += gen.uniform(0LL, A_ABS_MAX / n);
+            ll b = a * a / 2 - dec;
+            printf("%d %lld\n", a, b);
+        } else {
+            int p = gen.uniform(-A_ABS_MAX, A_ABS_MAX);
+            printf("%d\n", p);
+        }
+    }
+    return 0;
+}

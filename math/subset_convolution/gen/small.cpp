75de1f8d42f42f0afd0db4f40ee166bbdfa77467
Kohei Morita <yosupo06@gmail.com>
2020-02-25 08:29:07 +0900

    add gen/small.cpp and allow n = 0

--- math/subset_convolution/gen/small.cpp
+++ math/subset_convolution/gen/small.cpp
@@ -9,7 +9,7 @@ int main(int, char* argv[]) {
     long long seed = atoll(argv[1]);
     auto gen = Random(seed);
 
-    int n = gen.uniform<int>(0, N_MAX);    
+    int n = seed % 3;
     printf("%d\n", n);
     for (int i = 0; i < (1 << n); i++) {
         int x = gen.uniform<int>(0, MOD - 1);
--- math/subset_convolution/verifier.cpp
+++ math/subset_convolution/verifier.cpp
@@ -4,7 +4,7 @@
 int main() {
     registerValidation();
 
-    int n = inf.readInt(1, N_MAX); // a
+    int n = inf.readInt(0, N_MAX);
     inf.readEoln();
     for (int i = 0; i < (1 << n); i++) {
         inf.readInt(0, MOD - 1);

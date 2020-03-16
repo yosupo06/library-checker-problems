731cf4def660c6e03d4cc29411d8f8d558d78fe3
37zigen <43110026+Maruoka842@users.noreply.github.com>
2020-03-16 20:50:26 +0900

    Add files via upload

---
+++ math/kth_root_mod/gen/max_random.cpp
@@ -0,0 +1,47 @@
+#include "random.h"
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+using ll = long long;
+
+vector<ll> enum_prime(ll l, ll r) {
+    vector<int> is_prime(r - l + 1, true);
+    for (ll i = 2; i * i <= r; i++) {
+        for (ll j = max(2 * i, (l + i - 1) / i * i); j <= r; j += i) {
+            assert(l <= j && j <= r);
+            is_prime[j - l] = false;
+        }
+    }
+    vector<ll> res;
+    for (ll i = l; i <= r; i++) {
+        if (2 <= i && is_prime[i - l]) res.push_back(i);
+    }
+    return res;
+}
+
+long long pow(long long a,long long n,long long p){
+  long long ret=1;
+  for(;n>0;n>>=1,a=a*a%p)if(n%2==1)ret=ret*a%p;
+  return ret;
+}
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    ll up = gen.uniform((ll)9e8, (ll)1e9);
+    vector<ll> primes = enum_prime(up - (ll)5e6, up);
+
+    int t = 5000;
+    printf("%d\n", t);
+    for (int i = 0; i < t; i++) {
+        ll p = primes[gen.uniform<ll>(0LL, primes.size() - 1)];
+        ll k = gen.uniform(std::max(0LL,p - 1 - up), p - 1);
+        ll y = pow(gen.uniform(0LL, p - 1), k, p);
+        printf("%lld %lld %lld\n", k, y, p);
+    }
+    return 0;
+}
---
+++ math/kth_root_mod/gen/random.cpp
@@ -0,0 +1,47 @@
+#include "random.h"
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+using ll = long long;
+
+vector<ll> enum_prime(ll l, ll r) {
+    vector<int> is_prime(r - l + 1, true);
+    for (ll i = 2; i * i <= r; i++) {
+        for (ll j = max(2 * i, (l + i - 1) / i * i); j <= r; j += i) {
+            assert(l <= j && j <= r);
+            is_prime[j - l] = false;
+        }
+    }
+    vector<ll> res;
+    for (ll i = l; i <= r; i++) {
+        if (2 <= i && is_prime[i - l]) res.push_back(i);
+    }
+    return res;
+}
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    ll up = gen.uniform((ll)5e6, (ll)1e9);
+    vector<ll> primes = enum_prime(up - (ll)5e6, up);
+    vector<ll> small_primes = enum_prime(2, 1000);
+
+    int t = gen.uniform(1, 5000);
+    printf("%d\n", t);
+    for (int i = 0; i < t; i++) {
+        ll p;
+        if (gen.uniform_bool())
+            p = primes[gen.uniform<ll>(0LL, primes.size() - 1)];
+        else
+            p = small_primes[gen.uniform<ll>(0LL, small_primes.size() - 1)];
+        
+        ll y = gen.uniform(0LL, p - 1);
+        ll k = gen.uniform(0LL, p - 1);
+        printf("%lld %lld %lld\n", k, y, p);
+    }
+    return 0;
+}

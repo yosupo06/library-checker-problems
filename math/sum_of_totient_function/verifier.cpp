d355998d3f3f95e6fd2001bb451ea8e72d2d6a5a
maru <maru@DESKTOP-QM6O4MG.localdomain>
2020-03-27 15:23:27 +0900

    simplify

--- math/sum_of_totient_function/verifier.cpp
+++ math/sum_of_totient_function/verifier.cpp
@@ -7,8 +7,7 @@ using namespace std;
 
 int main() {
     registerValidation();
-    long long N = inf.readLong(1, N_MAX);
-    N++;N--;
+    inf.readLong(1, N_MAX);
     inf.readChar('\n');
     inf.readEof();
 }

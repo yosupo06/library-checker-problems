ad2e4b20e9053955710fdbe62db0d90c9e8fc6d1
maru <maru@DESKTOP-QM6O4MG.localdomain>
2020-03-28 19:39:20 +0900

    N=m^2,m(m+1)のテストケース

---
+++ math/sum_of_totient_function/gen/boundaryA.cpp
@@ -0,0 +1,24 @@
+#include "random.h"
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include "../params.h"
+
+using namespace std;
+using ll = long long;
+
+ll f(ll n) {
+  ll ret = 1;
+  while((ret+1)*(ret+1)<=n)++ret;
+  return ret*ret;
+}
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    ll N = gen.uniform<ll>(10LL, N_MAX);
+    printf("%lld\n", N);
+    return 0;
+}

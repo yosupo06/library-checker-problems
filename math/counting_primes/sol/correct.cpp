a9e9f68ca69996308c3119eae5d1914a9e157a88
tko919 <61387362+tko919@users.noreply.github.com>
2020-04-05 03:46:00 +0900

    Update correct.cpp

--- math/counting_primes/sol/correct.cpp
+++ math/counting_primes/sol/correct.cpp
@@ -9,7 +9,6 @@ using namespace std;
 #define rrep(i,a,b) for(int i=(a);i>(b);i--)
 #define ALL(v) (v).begin(),(v).end()
 typedef long long int ll;
-const int inf = 0x3fffffff; const ll INF = 0x1fffffffffffffff; const double eps=1e-12;
 template<class T> inline bool chmax(T& a,T b){ if(a<b){a=b;return 1;}return 0; }
 template<class T> inline bool chmin(T& a,T b){ if(a>b){a=b;return 1;}return 0; }
 //end

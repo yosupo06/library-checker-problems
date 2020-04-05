7fd92df24232d6a3b83f61172f7d2f2ed15ade11
tko919 <61387362+tko919@users.noreply.github.com>
2020-04-06 00:52:39 +0900

    Add files via upload

---
+++ math/subset_sum/gen/many_sets.cpp
@@ -0,0 +1,22 @@
+#include "random.h"
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include "../params.h"
+
+using namespace std;
+using ll = long long;
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    int N=T_MAX,T=T_MAX;
+    printf("%d %d\n", N,T);
+    for(int i=0;i<N;i++){
+        printf("%d",gen.uniform(1,3));
+        if(i!=N-1)printf(" ");
+    } printf("\n");
+    return 0;
+}
---
+++ math/subset_sum/gen/small.cpp
@@ -0,0 +1,22 @@
+#include "random.h"
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include "../params.h"
+
+using namespace std;
+using ll = long long;
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    int N=1'000,T=1'000;
+    printf("%d %d\n", N,T);
+    for(int i=0;i<N;i++){
+        printf("%d",gen.uniform(1,T));
+        if(i!=N-1)printf(" ");
+    } printf("\n");
+    return 0;
+}

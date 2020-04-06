b26c9ab3687998c6f862ddaa7a27b5470e6533c9
tko919 <61387362+tko919@users.noreply.github.com>
2020-04-06 14:03:07 +0900

    Update many_sets.cpp
    fix N setting

--- math/subset_sum/gen/many_sets.cpp
+++ math/subset_sum/gen/many_sets.cpp
@@ -12,7 +12,7 @@ int main(int, char* argv[]) {
     long long seed = atoll(argv[1]);
     auto gen = Random(seed);
 
-    int N=T_MAX,T=T_MAX;
+    int N=N_MAX,T=T_MAX;
     printf("%d %d\n", N,T);
     for(int i=0;i<N;i++){
         printf("%d",gen.uniform(1,3));

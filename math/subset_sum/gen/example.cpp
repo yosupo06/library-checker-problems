5aea5667b3e3240260e46d77a8458d7e85d9d500
tko919 <61387362+tko919@users.noreply.github.com>
2020-04-06 14:02:36 +0900

    Update example.cpp

--- math/subset_sum/gen/example.cpp
+++ math/subset_sum/gen/example.cpp
@@ -7,16 +7,7 @@
 using namespace std;
 using ll = long long;
 
-int main(int, char* argv[]) {
-
-    long long seed = atoll(argv[1]);
-    auto gen = Random(seed);
-
-    int N=5,T=5;
-    printf("%d %d\n", N,T);
-    for(int i=0;i<N;i++){
-        printf("%d",gen.uniform(1,T));
-        if(i!=N-1)printf(" ");
-    } printf("\n");
+int main() {
+    printf("5 3\n1 1 2 2 3\n");
     return 0;
 }

ea744f075483257941d9a549fa331341a131f9a1
maru <maru@DESKTOP-QM6O4MG.localdomain>
2020-04-12 17:54:46 +0900

    pow of formal power series

---
+++ math/pow_of_formal_power_series/gen/monomial.cpp
@@ -0,0 +1,24 @@
+#include "random.h"
+#include <iostream>
+#include "../params.h"
+
+using namespace std;
+using ll = long long;
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    int n = N_MAX;
+    int q = M_MAX;
+    int k = gen.uniform(0, (n - 1) / 2) * 2;
+    k += seed % 2;
+    printf("%d %d\n", n, q);
+    for (int i = 0; i < n; i++) {
+        printf("%d", (i == k ? 1 : 0));
+        if (i != n - 1) printf(" ");
+    }
+    printf("\n");
+    return 0;
+}
---
+++ math/pow_of_formal_power_series/task.md
@@ -0,0 +1,27 @@
+Problem Statement (Japanese) / 問題文 (日本語)
+---------
+f(x):=\sum_{i=0}^{N-1} a_i x^iとします。
+f(x)^M \bmod x^{N}を998244353で割った余り\sum_{i=0}^{N-1} b_ix^iを求めてください。
+
+Constraints / 制約
+---------
+
+- $1 \leq N \leq {{param N_MAX}}$
+- $1 \leq M \leq {{param M_MAX}}$
+- $0 \leq a_i < 998244353$
+- $1 \leq M \leq 10^9$
+
+Input / 入力
+---------
+
+```
+$N$ $M$
+$a_0$ $a_1$ $\ldots$ $a_{N-1}$
+```
+
+Sample / サンプル
+---------
+
+{{example example_00}}
+
+{{example example_01}}

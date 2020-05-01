a171be44f036e979a1065aec2d02145a3240f6cd
maspypy <45988013+maspypy@users.noreply.github.com>
2020-05-02 02:55:30 +0900

    Update math/polynomial_taylor_shift/verifier.cpp
    Co-authored-by: yosupo06 <yosupo06@gmail.com>

--- math/polynomial_taylor_shift/verifier.cpp
+++ math/polynomial_taylor_shift/verifier.cpp
@@ -7,7 +7,7 @@ int main()
 {
     registerValidation();
     int n = inf.readInt(1, N_MAX);
-    inf.readChar('\n');
+    inf.readChar(' ');
     inf.readInt(0, MOD - 1);
     inf.readChar('\n');
     for (int i = 0; i < n; i++)

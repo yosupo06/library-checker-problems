7bfe2cc85147f6a2e9641e6bc3f4ebe36cba0f6b
maspypy <maspypy@gmail.com>
2020-05-01 15:54:31 +0900

    add generator

---
+++ math/polynomial_taylor_shift/gen/small.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include "../params.h"
+#include "random.h"
+
+using namespace std;
+
+int main(int, char *argv[])
+{
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    int n = seed + 1;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        a[i] = gen.uniform(0ll, MOD - 1);
+    }
+    int c = gen.uniform(0ll, MOD - 1);
+
+    printf("%d\n", n);
+    printf("%d\n", c);
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d", a[i]);
+        if (i != n - 1)
+            printf(" ");
+    }
+    printf("\n");
+    return 0;
+}

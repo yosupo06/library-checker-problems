89efaeb203acbf17c7d2ad418904c783398759d0
Kohei Morita <yosupo06@gmail.com>
2020-05-04 11:30:48 +0900

    fix test_mingw

--- test_mingw.py
+++ test_mingw.py
@@ -8,9 +8,26 @@ import generate
 import json
 from logging import Logger, basicConfig, getLogger
 from os import getenv
-from typing import Dict
+from typing import Dict, Iterator
 
 logger = getLogger(__name__)  # type: Logger
+
+def list_depending_files(self) -> Iterator[Path]:
+    for path in list(self.libdir.glob('common/**/*')):
+        if (self.basedir / 'in').exists() and (self.basedir / 'in').resolve() in path.resolve().parents:
+            continue
+        if (self.basedir / 'out').exists() and (self.basedir / 'out').resolve() in path.resolve().parents:
+            continue
+        if not path.is_file():
+            continue
+        if os.access(str(path), os.X_OK):
+            continue  # ignore directries and compiled binaries
+        if path.name.endswith('.html'):
+            continue  # ignore generated HTML files
+        if path.name == 'params.h':
+            continue  # ignore generated params.h
+        yield path
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -34,6 +51,7 @@ def main():
     logger.info(problem_name)
     logger.info(problem_version)
     logger.info(list(problem.list_depending_files()))
+    logger.info(list(Path('.').glob('common/**/*')))
 
 if __name__ == '__main__':
     main()

da7e3282ff944086574bc348a013b2c3ac7d3136
sansen0796 <ymkw.snsn@gmail.com>
2020-05-07 15:40:11 +0900

    fix hash, rollback line random

--- graph/lca/gen/random.cpp
+++ graph/lca/gen/random.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <tuple>
 #include "random.h"
 #include "../params.h"
 
@@ -7,11 +6,12 @@ using namespace std;
 
 int main(int, char* argv[]) {
 
+
     long long seed = atoll(argv[1]);
     auto gen = Random(seed);
 
-    int n = gen.uniform<int>(N_MIN, N_MAX);
-    int q = gen.uniform<int>(Q_MIN, Q_MAX);
+    int n = gen.uniform(N_MIN, N_MAX);
+    int q = gen.uniform(Q_MIN, Q_MAX);
     printf("%d %d\n", n, q);
     for (int i = 1; i < n; i++) {
         printf("%d", gen.uniform(0, i - 1));
@@ -20,7 +20,10 @@ int main(int, char* argv[]) {
     printf("\n");
     for (int i = 0; i < q; i++) {
         int u, v;
-        std::tie(u, v) = gen.uniform_pair<int>(0, n - 1);
+        do {
+            u = gen.uniform(0, n - 1);
+            v = gen.uniform(0, n - 1);
+        } while (!(u < v));
         printf("%d %d\n", u, v);
     }
     return 0;

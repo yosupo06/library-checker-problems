4dea3aec2eaa5650c3471fc7bd7d04c42bddbfe3
sansen0796 <ymkw.snsn@gmail.com>
2020-05-10 09:40:23 +0900

    hash, info, verifer, task, generator, hand

--- graph/assignment/gen/hand_minus.cpp
+++ graph/assignment/gen/hand_minus.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
-#include "random.h"
+#include <vector>
 #include "../params.h"
 
 using namespace std;
 
-int main(int, char* argv[]) {
-
-
-    long long seed = atoll(argv[1]);
-    auto gen = Random(seed);
+int main(int, char**) {
 
     int n = N_MAX;
     vector<vector<int>> a(n, vector<int>(n));
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            a[i][j] = gen.uniform(-A_ABS_MAX, A_ABS_MAX);
+            a[i][j] = -(i * (i + 1) + j * (j + 1));
         }
     }
-
 
     printf("%d\n", n);
     for (int i = 0; i < n; i++) {

e0b760183ad453b8564fe253cf1d61845a15520d
Kohei Morita <yosupo06@gmail.com>
2020-05-24 13:42:59 +0900

    fix #376: add associative_array

---
+++ datastructure/associative_array/gen/max_random.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include "random.h"
+#include "../params.h"
+using namespace std;
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    int Q = Q_MAX;
+    printf("%d\n", Q);
+    int K = gen.uniform(1, 1000000);
+    vector<long long> keys(K);
+    for (auto& x: keys) {
+        x = gen.uniform(0LL, K_AND_V_MAX);
+    }
+    
+    for (int i = 0; i < Q; i++) {
+        bool type = gen.uniform_bool();
+        if (!type) {
+            long long key = keys[gen.uniform(0, K - 1)];
+            long long val = gen.uniform(0LL, K_AND_V_MAX);
+            printf("0 %lld %lld\n", key, val);
+        } else {
+            long long key = keys[gen.uniform(0, K - 1)];
+            printf("1 %lld\n", key);
+        }
+    }
+    
+    return 0;
+}
---
+++ datastructure/associative_array/gen/unordered_map_killer.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+#include <tuple>
+#include "random.h"
+#include "../params.h"
+using namespace std;
+
+/*
+このコードでMAGICが計算可能 参考: https://kimiyuki.net/blog/2017/03/08/unordered-map-hash-collision/
+
+#include <iostream>
+#include <unordered_map>
+using ll = long long;
+using namespace std;
+int main() {
+    int m = 1000000;
+    unordered_map<int,int> f;
+    int old = -1;
+    for (int i = 0; i < m; i++) {
+        int now = int(f.bucket_count());
+        if (old != now) {
+            cout << old << " -> " << now << endl;
+            old = now;
+        }
+        f[i] = i;
+    }
+    return 0;
+}
+*/
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    // magic for g++, clang++(OS X)
+    int MAGIC = vector<int>({256279, 205759})[seed % 2];
+
+    int Q = Q_MAX;
+    printf("%d\n", Q);
+
+    vector<long long> keys;
+    for (long long i = 0; i <= K_AND_V_MAX; i += MAGIC) {
+        keys.push_back(i);
+        if (int(keys.size()) == 1'000'000) break;
+    }
+
+    int K = int(keys.size());
+    
+    for (int i = 0; i < Q; i++) {
+        bool type = gen.uniform_bool();
+        if (!type) {
+            long long key = keys[i % K];
+            long long val = gen.uniform(0LL, K_AND_V_MAX);
+            printf("0 %lld %lld\n", key, val);
+        } else {
+            long long key = keys[i % K];
+            printf("1 %lld\n", key);
+        }
+    }
+    
+    return 0;
+}

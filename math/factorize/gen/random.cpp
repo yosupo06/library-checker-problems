01ae500a34198ae39d65d38443292397cfc5cf65
Kohei Morita <yosupo06@gmail.com>
2020-08-24 21:01:03 +0900

    use param for factorize and increase queries of big2

--- math/factorize/gen/random.cpp
+++ math/factorize/gen/random.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "random.h"
+#include "../params.h"
 
 using namespace std;
 
@@ -9,10 +10,10 @@ int main(int, char* argv[]) {
     long long seed = atoll(argv[1]);
     auto gen = Random(seed);
 
-    int q = gen.uniform(1, 100);
+    int q = gen.uniform(1LL, MAX_Q);
     vector<long long> a(q);
     for (int i = 0; i < q; i++) {
-        a[i] = gen.uniform(1LL, 1'000'000'000'000'000'000LL);
+        a[i] = gen.uniform(1LL, MAX_A);
     }
 
     printf("%d\n", q);

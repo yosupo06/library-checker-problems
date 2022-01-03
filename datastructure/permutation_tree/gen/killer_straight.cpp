6a89e96480ba34eeb88d237f72c30283701f584a
hotman78 <hotmanww@gmail.com>
2022-01-03 14:30:43 +0900

    add killer case

---
+++ datastructure/permutation_tree/gen/killer_straight.cpp
@@ -0,0 +1,36 @@
+#include "random.h"
+#include "../params.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include<iostream>
+
+int main(int, char* argv[]) {
+    long long seed = std::atoll(argv[1]);
+    auto gen = Random(seed);
+    const int n = N_MAX;
+    std::vector<int>p(n);
+    auto dfs=[&](auto dfs,int l,int r,int mn,int mx,bool rev)->void{
+        if(r-l<5){
+            auto tmp=gen.perm<int>(r-l);
+            for(int i=0;i<r-l;++i)p[l+i]=tmp[i]+mn;
+        }else{
+            int m=(l+r)/2;
+            if(rev){
+                dfs(dfs,l,m,mn+(r-m),mx,0);
+                dfs(dfs,m,r,mn,mn+(r-m),0);
+            }else{
+                dfs(dfs,l,m,mn,mn+(m-l),1);
+                dfs(dfs,m,r,mn+(m-l),mx,1);
+            }
+        }
+    };
+    dfs(dfs,0,n,0,n,0);
+    std::cout<<n<<'\n';
+    for(int i=0;i<n;++i){
+        if(i)std::cout<<" ";
+        std::cout<<p[i];
+    }
+    std::cout<<'\n';
+}

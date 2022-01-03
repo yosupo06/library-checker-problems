1490eb06b737603587de450d9ab8c6312220c35a
hotman78 <hotmanww@gmail.com>
2022-01-03 14:30:27 +0900

    fix solution

--- datastructure/permutation_tree/sol/correct.cpp
+++ datastructure/permutation_tree/sol/correct.cpp
@@ -10,11 +10,16 @@ int main(){
     perm_tree<int>pt(p);
     auto root=pt.get();
     using np=decltype(root);
+    vector<tuple<int,int,string>>v;
     auto dfs=[&](auto dfs,np t)->void{
-        cout<<t->l<<" "<<t->r<<(t->l+1==t->r?"leaf":(t->is_join?"join":"cut"))<<endl;
+        v.emplace_back(t->l,t->r,(t->is_join?"join":"cut"));
         for(auto e:t->ch){
             dfs(dfs,e);
         }
     };
     dfs(dfs,root);
+    cout<<v.size()<<endl;
+    for(auto [l,r,s]:v){
+        cout<<l<<" "<<r<<" "<<s<<endl;
+    }
 }

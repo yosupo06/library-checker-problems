1f3700acc430ba65f268793e6491d06e986fee12
lrvideckis <lukevideckis@gmail.com>
2022-02-08 22:37:36 -0800

    it now compiles

--- graph/bipartitematching/gen/heuristic_matching_killer.cpp
+++ graph/bipartitematching/gen/heuristic_matching_killer.cpp
@@ -1,46 +1,42 @@
 #include <iostream>
-#include <set>
+#include <vector>
 
-#include "../../../common/random.h"
 #include "../params.h"
 
 using namespace std;
 
-int main(int, char* argv[]) {
+int main() {
 
-	long long seed = atoll(argv[1]);
-	auto gen = Random(seed);
-
-	int l = gen.uniform(0, L_MAX - 1);
-	int r = l;
+	int l = L_MAX;
+	int r = R_MAX;
 	int m = M_MAX;
 
 	using P = pair<int, int>;
-	set<P> edges;
+	vector<P> edges;
 
 	int k = l / 8 * 2;
 	for(int i = 0; i < k; i++) {
-		edges.insert({i, i});
+		edges.push_back({i, i});
 	}
 	for(int i = 0; i < k-1; i++) {
-		edges.insert({i+1, i});
+		edges.push_back({i+1, i});
 	}
 	for(int i = 0; i < k/2; i++) {
-		edges.insert({i, k+2*i});
-		edges.insert({2*k+2*i, k+2*i});
-		edges.insert({2*k+2*i, 3*k+2*i});
-		edges.insert({k+2*i, i});
-		edges.insert({k+2*i, 2*k+2*i});
-		edges.insert({3*k+2*i, 2*k+2*i});
+		edges.push_back({i, k+2*i});
+		edges.push_back({2*k+2*i, k+2*i});
+		edges.push_back({2*k+2*i, 3*k+2*i});
+		edges.push_back({k+2*i, i});
+		edges.push_back({k+2*i, 2*k+2*i});
+		edges.push_back({3*k+2*i, 2*k+2*i});
 	}
 	for(int j = 0; j < k/2; j++) {
 		int i = k/2-1-j;
-		edges.insert({k/2+j, k+2*i+1});
-		edges.insert({2*k+2*i+1, k+2*i+1});
-		edges.insert({2*k+2*i+1, 3*k+2*i+1});
-		edges.insert({k+2*i+1, k/2+j});
-		edges.insert({k+2*i+1, 2*k+2*i+1});
-		edges.insert({3*k+2*i+1, 2*k+2*i+1});
+		edges.push_back({k/2+j, k+2*i+1});
+		edges.push_back({2*k+2*i+1, k+2*i+1});
+		edges.push_back({2*k+2*i+1, 3*k+2*i+1});
+		edges.push_back({k+2*i+1, k/2+j});
+		edges.push_back({k+2*i+1, 2*k+2*i+1});
+		edges.push_back({3*k+2*i+1, 2*k+2*i+1});
 	}
 
 	m = int(edges.size());

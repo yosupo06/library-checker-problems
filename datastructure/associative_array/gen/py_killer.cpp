644650baf1020c908e8cec645cee8f0730ea537f
shirotsume4 <81575990+shirotsume4@users.noreply.github.com>
2022-05-12 19:10:10 +0900

    Add files via upload

---
+++ datastructure/associative_array/gen/py_killer.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include "random.h"
+#include "../params.h"
+using namespace std;
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+    long long MAXI = 1e18ll;
+    int Q = 1000000;
+    long long mask = (1 << 50) - 1;
+
+    printf("%d\n", Q);
+
+    int query0_col = 400000;
+    int query0_one = 400000;
+    int query1 = 200000;
+
+    printf("%d %lld\n", 0, mask + 2);
+
+    long long int k = 6;
+    for(int i = 1; i < query0_col; i++){
+        long long v = gen.uniform<long long>(0, MAXI);
+        printf("%d %lld %lld\n", 0, k, v);
+        k = k * 5 + 1;
+        k &= mask;
+    }
+
+    for(int i = 0; i < query0_one; i++){
+        long long v = gen.uniform<long long>(0, MAXI);
+        printf("%d %lld %lld\n", 0, 1ll, v);
+    }
+
+    
+    for(int i = 0; i < query1; i++){
+        printf("%d %d", 1, i);
+    }
+    return 0;
+}

78ac34c4273d9d1665edec67b0fd018fddda4436
maspypy <maspypy.gmail.com>
2022-06-27 15:54:14 +0900

    add problem

---
+++ datastructure/static_rectangle_add_rectangle_sum/sol/naive.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <vector>
+#include <cassert>
+
+using namespace std;
+using ll = long long;
+
+template <unsigned int mod>
+struct modint {
+  static constexpr bool is_modint = true;
+  unsigned int val;
+  constexpr modint(const ll val = 0) noexcept
+      : val(val >= 0 ? val % mod : (mod - (-val) % mod) % mod) {}
+  bool operator<(const modint &other) const {
+    return val < other.val;
+  } // To use std::map
+  modint &operator+=(const modint &p) {
+    if ((val += p.val) >= mod) val -= mod;
+    return *this;
+  }
+  modint &operator-=(const modint &p) {
+    if ((val += mod - p.val) >= mod) val -= mod;
+    return *this;
+  }
+  modint &operator*=(const modint &p) {
+    val = (unsigned int)(1LL * val * p.val % mod);
+    return *this;
+  }
+  modint &operator/=(const modint &p) {
+    *this *= p.inverse();
+    return *this;
+  }
+  modint operator-() const { return modint(get_mod() - val); }
+  modint operator+(const modint &p) const { return modint(*this) += p; }
+  modint operator-(const modint &p) const { return modint(*this) -= p; }
+  modint operator*(const modint &p) const { return modint(*this) *= p; }
+  modint operator/(const modint &p) const { return modint(*this) /= p; }
+  bool operator==(const modint &p) const { return val == p.val; }
+  bool operator!=(const modint &p) const { return val != p.val; }
+  modint inverse() const {
+    int a = val, b = mod, u = 1, v = 0, t;
+    while (b > 0) {
+      t = a / b;
+      swap(a -= t * b, b), swap(u -= t * v, v);
+    }
+    return modint(u);
+  }
+  modint pow(int64_t n) const {
+    modint ret(1), mul(val);
+    while (n > 0) {
+      if (n & 1) ret *= mul;
+      mul *= mul;
+      n >>= 1;
+    }
+    return ret;
+  }
+  static constexpr unsigned int get_mod() { return mod; }
+};
+
+using mint = modint<998244353>;
+
+mint A[111][111];
+
+int main() {
+  int N, Q;
+  scanf("%d %d", &N, &Q);
+  for (int n = 0; n < N; ++n) {
+    ll l, d, r, u, w;
+    scanf("%lld %lld %lld %lld %lld", &l, &d, &r, &u, &w);
+    assert(max(r, u) < 111);
+    for (int x = l; x < r; ++x) {
+      for (int y = d; y < u; ++y) { A[x][y] += mint(w); }
+    }
+  }
+
+  for (int q = 0; q < Q; ++q) {
+    mint sm = 0;
+    ll l, d, r, u;
+    scanf("%lld %lld %lld %lld", &l, &d, &r, &u);
+    assert(max(r, u) < 111);
+    for (int x = l; x < r; ++x) {
+      for (int y = d; y < u; ++y) { sm += A[x][y]; }
+    }
+    printf("%d\n", sm.val);
+  }
+
+  return 0;
+}
---
+++ datastructure/static_rectangle_add_rectangle_sum/task.md
@@ -0,0 +1,50 @@
+## @{keyword.statement}
+
+@{lang.en}
+You are given a $@{param.X_MAX} \times @{param.Y_MAX}$ matrix $A = (A_{xy})$ ($0\leq x < @{param.X_MAX}$, $0\leq y < @{param.Y_MAX}$). 
+Initially, all entries $A_{xy}$ equal $0$. 
+
+First, process following $N$ queries. 
+
+- `$l$ $d$ $r$ $u$ $w$`: Add $w$ to all entries $A_{xy}$ satistying $l\leq x < r$ and $d\leq y < u$. 
+
+Then, process following $Q$ queries. 
+
+- `$l$ $d$ $r$ $u$ $w$`: Calculate sum of entries $A_{xy}$ satistying $l\leq x < r$ and $d\leq y < u$, and print it modulo $@{param.MOD}$. 
+
+@{lang.ja}
+$@{param.X_MAX} \times @{param.Y_MAX}$ 行列 $A = (A_{xy})$ ($0\leq x < @{param.X_MAX}$, $0\leq y < @{param.Y_MAX}$) があります。
+はじめ、すべての $x, y$ に対して $A_{xy} = 0$ です。
+
+まず、次のクエリを $N$ 個処理してください。
+
+- `$l$ $d$ $r$ $u$ $w$`: $l\leq x < r$ かつ $d\leq y < u$ を満たすすべての成分 $A_{x,y}$ に $w$ を加算する。
+
+その後、次のクエリを $Q$ 個処理してください。
+
+- `$l$ $d$ $r$ $u$`: $l\leq x < r$ かつ $d\leq y < u$ を満たすすべての成分 $A_{x,y}$ の総和を $@{param.MOD}$ で割った余りを出力する。
+
+@{lang.end}
+
+
+## @{keyword.constraints}
+
+- $@{param.N_MIN} \leq N \leq @{param.N_MAX}$
+- $@{param.Q_MIN} \leq Q \leq @{param.Q_MAX}$
+- $@{param.X_MIN} \leq l_i < r_i \leq @{param.X_MAX}$
+- $@{param.Y_MIN} \leq d_i < u_i \leq @{param.Y_MAX}$
+- $0 \leq w_i < @{param.MOD}$
+
+## @{keyword.input}
+
+~~~
+$N$ $Q$
+$l_0$ $d_0$ $r_0$ $u_0$ $w_0$
+$\vdots$
+$l_{N-1}$ $d_{N-1}$ $r_{N-1}$ $u_{N-1}$ $w_{N-1}$
+$l_0$ $d_0$ $r_0$ $u_0$ $w_0$
+$\vdots$
+$l_{Q-1}$ $d_{Q-1}$ $r_{Q-1}$ $u_{Q-1}$
+~~~
+
+@{example.example_00}

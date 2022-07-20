4ea173dd82f67a1a608331a823a6cbc44f296159
maspypy <maspypy.gmail.com>
2022-07-20 14:30:30 +0900

    reduce template / avoid bits

--- graph/jump_on_tree/sol/correct.cpp
+++ graph/jump_on_tree/sol/correct.cpp
@@ -1,5 +1,7 @@
-#line 1 "library/my_template.hpp"
-#include <bits/stdc++.h>
+#include <vector>
+#include <cassert>
+#include <algorithm>
+#include <cstdio>
 
 using namespace std;
 
@@ -12,29 +14,6 @@ using i128 = __int128;
 
 template <class T>
 using vc = vector<T>;
-template <class T>
-using vvc = vector<vc<T>>;
-template <class T>
-using vvvc = vector<vvc<T>>;
-template <class T>
-using vvvvc = vector<vvvc<T>>;
-template <class T>
-using vvvvvc = vector<vvvvc<T>>;
-template <class T>
-using pq = priority_queue<T>;
-template <class T>
-using pqg = priority_queue<T, vector<T>, greater<T>>;
-
-#define vec(type, name, ...) vector<type> name(__VA_ARGS__)
-#define vv(type, name, h, ...) \
-  vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
-#define vvv(type, name, h, w, ...)   \
-  vector<vector<vector<type>>> name( \
-      h, vector<vector<type>>(w, vector<type>(__VA_ARGS__)))
-#define vvvv(type, name, a, b, c, ...)       \
-  vector<vector<vector<vector<type>>>> name( \
-      a, vector<vector<vector<type>>>(       \
-             b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
 
 // https://trap.jp/post/1224/
 #define FOR1(a) for (ll _ = 0; _ < ll(a); ++_)
@@ -50,7 +29,6 @@ using pqg = priority_queue<T, vector<T>, greater<T>>;
 #define FOR_R(...) \
   overload4(__VA_ARGS__, FOR4_R, FOR3_R, FOR2_R, FOR1_R)(__VA_ARGS__)
 
-#define FOR_subset(t, s) for (ll t = s; t >= 0; t = (t == 0 ? -1 : (t - 1) & s))
 #define all(x) x.begin(), x.end()
 #define len(x) ll(x.size())
 #define elif else if
@@ -61,77 +39,6 @@ using pqg = priority_queue<T, vector<T>, greater<T>>;
 #define fi first
 #define se second
 
-#define stoi stoll
-
-template <typename T>
-T SUM(vector<T> &A) {
-  T sum = T(0);
-  for (auto &&a: A) sum += a;
-  return sum;
-}
-
-#define MIN(v) *min_element(all(v))
-#define MAX(v) *max_element(all(v))
-#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
-#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
-#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end())
-
-int popcnt(int x) { return __builtin_popcount(x); }
-int popcnt(u32 x) { return __builtin_popcount(x); }
-int popcnt(ll x) { return __builtin_popcountll(x); }
-int popcnt(u64 x) { return __builtin_popcountll(x); }
-// (0, 1, 2, 3, 4) -> (-1, 0, 1, 1, 2)
-int topbit(int x) { return (x == 0 ? -1 : 31 - __builtin_clz(x)); }
-int topbit(u32 x) { return (x == 0 ? -1 : 31 - __builtin_clz(x)); }
-int topbit(ll x) { return (x == 0 ? -1 : 63 - __builtin_clzll(x)); }
-int topbit(u64 x) { return (x == 0 ? -1 : 63 - __builtin_clzll(x)); }
-// (0, 1, 2, 3, 4) -> (-1, 0, 1, 0, 2)
-int lowbit(int x) { return (x == 0 ? -1 : __builtin_ctz(x)); }
-int lowbit(u32 x) { return (x == 0 ? -1 : __builtin_ctz(x)); }
-int lowbit(ll x) { return (x == 0 ? -1 : __builtin_ctzll(x)); }
-int lowbit(u64 x) { return (x == 0 ? -1 : __builtin_ctzll(x)); }
-
-template <typename T, typename U>
-T ceil(T x, U y) {
-  return (x > 0 ? (x + y - 1) / y : x / y);
-}
-
-template <typename T, typename U>
-T floor(T x, U y) {
-  return (x > 0 ? x / y : (x - y + 1) / y);
-}
-
-template <typename T, typename U>
-pair<T, T> divmod(T x, U y) {
-  T q = floor(x, y);
-  return {q, x - q * y};
-}
-
-ll binary_search(function<bool(ll)> check, ll ok, ll ng) {
-  assert(check(ok));
-  while (abs(ok - ng) > 1) {
-    auto x = (ng + ok) / 2;
-    if (check(x))
-      ok = x;
-    else
-      ng = x;
-  }
-  return ok;
-}
-
-template <typename F>
-double binary_search_real(F check, double ok, double ng, int iter = 100) {
-  FOR(iter) {
-    double x = (ok + ng) / 2;
-    if (check(x)) {
-      ok = x;
-    } else {
-      ng = x;
-    }
-  }
-  return (ok + ng) / 2;
-}
-
 template <class T, class S>
 inline bool chmax(T &a, const S &b) {
   return (a < b ? a = b, 1 : 0);
@@ -140,345 +47,6 @@ template <class T, class S>
 inline bool chmin(T &a, const S &b) {
   return (a > b ? a = b, 1 : 0);
 }
-
-vi s_to_vi(const string &S, char first_char) {
-  vi A(S.size());
-  FOR(i, S.size()) { A[i] = S[i] - first_char; }
-  return A;
-}
-
-template <typename T>
-vector<T> cumsum(vector<T> &A, int off = 1) {
-  int N = A.size();
-  vector<T> B(N + 1);
-  FOR(i, N) { B[i + 1] = B[i] + A[i]; }
-  if (off == 0) B.erase(B.begin());
-  return B;
-}
-
-template <typename CNT, typename T>
-vc<CNT> bincount(const vc<T> &A, int size) {
-  vc<CNT> C(size);
-  for (auto &&x: A) { ++C[x]; }
-  return C;
-}
-
-template <typename T>
-vector<int> argsort(const vector<T> &A) {
-  // stable
-  vector<int> ids(A.size());
-  iota(all(ids), 0);
-  sort(all(ids),
-       [&](int i, int j) { return A[i] < A[j] || (A[i] == A[j] && i < j); });
-  return ids;
-}
-
-// A[I[0]], A[I[1]], ...
-template <typename T>
-vc<T> rearrange(const vc<T> &A, const vc<int> &I) {
-  int n = len(A);
-  assert(len(I) == n);
-  vc<T> B(n);
-  FOR(i, n) B[i] = A[I[i]];
-  return B;
-}
-#line 1 "library/other/io.hpp"
-// based on yosupo's fastio
-#include <unistd.h>
-
-namespace detail {
-template <typename T, decltype(&T::is_modint) = &T::is_modint>
-std::true_type check_value(int);
-template <typename T>
-std::false_type check_value(long);
-} // namespace detail
-
-template <typename T>
-struct is_modint : decltype(detail::check_value<T>(0)) {};
-template <typename T>
-using is_modint_t = enable_if_t<is_modint<T>::value>;
-template <typename T>
-using is_not_modint_t = enable_if_t<!is_modint<T>::value>;
-
-struct Scanner {
-  FILE *fp;
-  char line[(1 << 15) + 1];
-  size_t st = 0, ed = 0;
-  void reread() {
-    memmove(line, line + st, ed - st);
-    ed -= st;
-    st = 0;
-    ed += fread(line + ed, 1, (1 << 15) - ed, fp);
-    line[ed] = '\0';
-  }
-  bool succ() {
-    while (true) {
-      if (st == ed) {
-        reread();
-        if (st == ed) return false;
-      }
-      while (st != ed && isspace(line[st])) st++;
-      if (st != ed) break;
-    }
-    if (ed - st <= 50) {
-      bool sep = false;
-      for (size_t i = st; i < ed; i++) {
-        if (isspace(line[i])) {
-          sep = true;
-          break;
-        }
-      }
-      if (!sep) reread();
-    }
-    return true;
-  }
-  template <class T, enable_if_t<is_same<T, string>::value, int> = 0>
-  bool read_single(T &ref) {
-    if (!succ()) return false;
-    while (true) {
-      size_t sz = 0;
-      while (st + sz < ed && !isspace(line[st + sz])) sz++;
-      ref.append(line + st, sz);
-      st += sz;
-      if (!sz || st != ed) break;
-      reread();
-    }
-    return true;
-  }
-  template <class T, enable_if_t<is_integral<T>::value, int> = 0>
-  bool read_single(T &ref) {
-    if (!succ()) return false;
-    bool neg = false;
-    if (line[st] == '-') {
-      neg = true;
-      st++;
-    }
-    ref = T(0);
-    while (isdigit(line[st])) { ref = 10 * ref + (line[st++] & 0xf); }
-    if (neg) ref = -ref;
-    return true;
-  }
-  template <class T, is_modint_t<T> * = nullptr>
-  bool read_single(T &ref) {
-    long long val = 0;
-    bool f = read_single(val);
-    ref = T(val);
-    return f;
-  }
-  bool read_single(double &ref) {
-    string s;
-    if (!read_single(s)) return false;
-    ref = std::stod(s);
-    return true;
-  }
-  bool read_single(char &ref) {
-    string s;
-    if (!read_single(s) || s.size() != 1) return false;
-    ref = s[0];
-    return true;
-  }
-  template <class T>
-  bool read_single(vector<T> &ref) {
-    for (auto &d: ref) {
-      if (!read_single(d)) return false;
-    }
-    return true;
-  }
-  template <class T, class U>
-  bool read_single(pair<T, U> &p) {
-    return (read_single(p.first) && read_single(p.second));
-  }
-  template <class A, class B, class C>
-  bool read_single(tuple<A, B, C> &p) {
-    return (read_single(get<0>(p)) && read_single(get<1>(p))
-            && read_single(get<2>(p)));
-  }
-  template <class A, class B, class C, class D>
-  bool read_single(tuple<A, B, C, D> &p) {
-    return (read_single(get<0>(p)) && read_single(get<1>(p))
-            && read_single(get<2>(p)) && read_single(get<3>(p)));
-  }
-  void read() {}
-  template <class H, class... T>
-  void read(H &h, T &... t) {
-    bool f = read_single(h);
-    assert(f);
-    read(t...);
-  }
-  Scanner(FILE *fp) : fp(fp) {}
-};
-
-struct Printer {
-  Printer(FILE *_fp) : fp(_fp) {}
-  ~Printer() { flush(); }
-
-  static constexpr size_t SIZE = 1 << 15;
-  FILE *fp;
-  char line[SIZE], small[50];
-  size_t pos = 0;
-  void flush() {
-    fwrite(line, 1, pos, fp);
-    pos = 0;
-  }
-  void write(const char &val) {
-    if (pos == SIZE) flush();
-    line[pos++] = val;
-  }
-  template <class T, enable_if_t<is_integral<T>::value, int> = 0>
-  void write(T val) {
-    if (pos > (1 << 15) - 50) flush();
-    if (val == 0) {
-      write('0');
-      return;
-    }
-    if (val < 0) {
-      write('-');
-      val = -val; // todo min
-    }
-    size_t len = 0;
-    while (val) {
-      small[len++] = char(0x30 | (val % 10));
-      val /= 10;
-    }
-    for (size_t i = 0; i < len; i++) { line[pos + i] = small[len - 1 - i]; }
-    pos += len;
-  }
-  void write(const string &s) {
-    for (char c: s) write(c);
-  }
-  void write(const char *s) {
-    size_t len = strlen(s);
-    for (size_t i = 0; i < len; i++) write(s[i]);
-  }
-  void write(const double &x) {
-    ostringstream oss;
-    oss << setprecision(15) << x;
-    string s = oss.str();
-    write(s);
-  }
-  void write(const long double &x) {
-    ostringstream oss;
-    oss << setprecision(15) << x;
-    string s = oss.str();
-    write(s);
-  }
-  template <class T, is_modint_t<T> * = nullptr>
-  void write(T &ref) {
-    write(ref.val);
-  }
-  template <class T>
-  void write(const vector<T> &val) {
-    auto n = val.size();
-    for (size_t i = 0; i < n; i++) {
-      if (i) write(' ');
-      write(val[i]);
-    }
-  }
-  template <class T, class U>
-  void write(const pair<T, U> &val) {
-    write(val.first);
-    write(' ');
-    write(val.second);
-  }
-  template <class A, class B, class C>
-  void write(const tuple<A, B, C> &val) {
-    auto &[a, b, c] = val;
-    write(a), write(' '), write(b), write(' '), write(c);
-  }
-  template <class A, class B, class C, class D>
-  void write(const tuple<A, B, C, D> &val) {
-    auto &[a, b, c, d] = val;
-    write(a), write(' '), write(b), write(' '), write(c), write(' '), write(d);
-  }
-  template <class A, class B, class C, class D, class E>
-  void write(const tuple<A, B, C, D, E> &val) {
-    auto &[a, b, c, d, e] = val;
-    write(a), write(' '), write(b), write(' '), write(c), write(' '), write(d),
-        write(' '), write(e);
-  }
-  template <class A, class B, class C, class D, class E, class F>
-  void write(const tuple<A, B, C, D, E, F> &val) {
-    auto &[a, b, c, d, e, f] = val;
-    write(a), write(' '), write(b), write(' '), write(c), write(' '), write(d),
-        write(' '), write(e), write(' '), write(f);
-  }
-  template <class T, size_t S>
-  void write(const array<T, S> &val) {
-    auto n = val.size();
-    for (size_t i = 0; i < n; i++) {
-      if (i) write(' ');
-      write(val[i]);
-    }
-  }
-  void write(i128 val) {
-    string s;
-    bool negative = 0;
-    if (val < 0) {
-      negative = 1;
-      val = -val;
-    }
-    while (val) {
-      s += '0' + int(val % 10);
-      val /= 10;
-    }
-    if (negative) s += "-";
-    reverse(all(s));
-    if (len(s) == 0) s = "0";
-    write(s);
-  }
-};
-
-Scanner scanner = Scanner(stdin);
-Printer printer = Printer(stdout);
-
-void flush() { printer.flush(); }
-void print() { printer.write('\n'); }
-template <class Head, class... Tail>
-void print(Head &&head, Tail &&... tail) {
-  printer.write(head);
-  if (sizeof...(Tail)) printer.write(' ');
-  print(forward<Tail>(tail)...);
-}
-
-void read() {}
-template <class Head, class... Tail>
-void read(Head &head, Tail &... tail) {
-  scanner.read(head);
-  read(tail...);
-}
-
-#define INT(...)   \
-  int __VA_ARGS__; \
-  read(__VA_ARGS__)
-#define LL(...)   \
-  ll __VA_ARGS__; \
-  read(__VA_ARGS__)
-#define STR(...)      \
-  string __VA_ARGS__; \
-  read(__VA_ARGS__)
-#define CHAR(...)   \
-  char __VA_ARGS__; \
-  read(__VA_ARGS__)
-#define DBL(...)      \
-  double __VA_ARGS__; \
-  read(__VA_ARGS__)
-
-#define VEC(type, name, size) \
-  vector<type> name(size);    \
-  read(name)
-#define VV(type, name, h, w)                     \
-  vector<vector<type>> name(h, vector<type>(w)); \
-  read(name)
-
-void YES(bool t = 1) { print(t ? "YES" : "NO"); }
-void NO(bool t = 1) { YES(!t); }
-void Yes(bool t = 1) { print(t ? "Yes" : "No"); }
-void No(bool t = 1) { Yes(!t); }
-void yes(bool t = 1) { print(t ? "yes" : "no"); }
-void no(bool t = 1) { yes(!t); }
-#line 3 "main.cpp"
-
-#line 2 "library/graph/base.hpp"
 
 template <typename T>
 struct Edge {
@@ -531,33 +99,6 @@ struct Graph {
     ++M;
   }
 
-  // wt, off
-  void read_tree(bool wt = false, int off = 1) { read_graph(N - 1, wt, off); }
-
-  void read_graph(int M, bool wt = false, int off = 1) {
-    for (int m = 0; m < M; ++m) {
-      INT(a, b);
-      a -= off, b -= off;
-      if (!wt) {
-        add(a, b);
-      } else {
-        T c;
-        read(c);
-        add(a, b, c);
-      }
-    }
-    build();
-  }
-
-  void read_parent(int off = 1) {
-    for (int v = N - 1; v >= 1; --v) {
-      INT(p);
-      p -= off;
-      add(p, v);
-    }
-    build();
-  }
-
   void build() {
     assert(!prepared);
     prepared = true;
@@ -580,26 +121,8 @@ struct Graph {
     assert(prepared);
     return {this, indptr[v], indptr[v + 1]};
   }
-
-  void debug() {
-    print("Graph");
-    if (!prepared) {
-      print("frm to cost id");
-      for (auto &&e: edges) print(e.frm, e.to, e.cost, e.id);
-    } else {
-      print("indptr", indptr);
-      print("frm to cost id");
-      FOR(v, N) for (auto &&e: (*this)[v]) print(e.frm, e.to, e.cost, e.id);
-    }
-  }
 };
-#line 3 "library/graph/hld.hpp"
 
-/*
-HL分解。O(N) 時間構築。
-LCA, LA などは O(logN) 時間。
-木以外、非連結でも使えるようにした。dfs順序や親がとれる。
-*/
 template <typename Graph>
 struct HLD {
   Graph &G;
@@ -759,34 +282,28 @@ struct HLD {
     up.insert(up.end(), all(down));
     return up;
   }
-
-  void debug() {
-    print("V", V);
-    print("LID", LID);
-    print("RID", RID);
-    print("parent", parent);
-    print("depth", depth);
-    print("head", head);
-    print("in_tree(edge)", in_tree);
-    print("root", root);
-  }
 };
-#line 5 "main.cpp"
 
 void solve() {
-  LL(N, Q);
+  int N, Q;
+  scanf("%d %d", &N, &Q);
   Graph<int, 0> G(N);
-  G.read_tree(0, 0);
+  FOR(N - 1) {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    G.add(a, b);
+  }
+  G.build();
+
   HLD<decltype(G)> hld(G);
   FOR(Q) {
-    LL(s, t, i);
-    print(hld.jump(s, t, i));
+    int s, t, i;
+    scanf("%d %d %d", &s, &t, &i);
+    printf("%d\n", hld.jump(s, t, i));
   }
 }
 
 signed main() {
-  cout << fixed << setprecision(15);
-
   ll T = 1;
   // LL(T);
   FOR(T) solve();

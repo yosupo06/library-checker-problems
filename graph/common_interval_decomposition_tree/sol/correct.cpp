8475debcaf2b088df3df6fc3f3ed24665fa21a42
hotman78 <hotmanww@gmail.com>
2022-08-12 14:24:17 +0900

    fix judge

--- graph/common_interval_decomposition_tree/sol/correct.cpp
+++ graph/common_interval_decomposition_tree/sol/correct.cpp
@@ -8,23 +8,27 @@ using namespace std;
 int main()
 {
     int n;
-    cin >> n;
+    scanf("%d", &n);
     vector<int> p(n);
     for (int i = 0; i < n; ++i)
-        cin >> p[i];
+        scanf("%d", &p[i]);
     perm_tree<int> pt(p);
     auto root = pt.get();
     using np = decltype(root);
-    vector<tuple<int, int, string>> v;
-    auto dfs = [&](auto dfs, np t) -> void {
-        v.emplace_back(t->l, t->r, (t->is_join ? "linear" : "prime"));
-        for (auto e : t->ch) {
-            dfs(dfs, e);
+    vector<tuple<int, int, int, string>> v;
+    auto dfs = [&](auto dfs, np t, int par) -> void
+    {
+        int now = v.size();
+        v.emplace_back(par, t->l, t->r, (t->is_join ? "linear" : "prime"));
+        for (auto e : t->ch)
+        {
+            dfs(dfs, e, now);
         }
     };
-    dfs(dfs, root);
+    dfs(dfs, root, -1);
     printf("%zu\n", v.size());
-    for (auto [l, r, s] : v) {
-        printf("%d %d %s\n", l, r-1, s.c_str());
+    for (auto [par, l, r, s] : v)
+    {
+        printf("%d %d %d %s\n", par, l, r - 1, s.c_str());
     }
 }

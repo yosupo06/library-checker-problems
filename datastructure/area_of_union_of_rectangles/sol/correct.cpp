c5b781822cbf2e649009b9314583108a9aa643b0
tko919 <laplace2758@gmail.com>
2022-10-19 22:26:56 +0900

    fix

--- datastructure/area_of_union_of_rectangles/sol/correct.cpp
+++ datastructure/area_of_union_of_rectangles/sol/correct.cpp
@@ -9,10 +9,7 @@ using namespace std;
 #define rep(i,a,b) for(int i=(int)(a);i<(int)(b);i++)
 #define ALL(v) (v).begin(),(v).end()
 using ll=long long int;
-const int inf = 0x3fffffff;
 const ll INF = 0x1fffffffffffffff;
-template<typename T>inline bool chmax(T& a,T b){if(a<b){a=b;return 1;}return 0;}
-template<typename T>inline bool chmin(T& a,T b){if(a>b){a=b;return 1;}return 0;}
 #line 5 "sol.cpp"
 
 #line 2 "library/DataStructure/lazysegtree.hpp"

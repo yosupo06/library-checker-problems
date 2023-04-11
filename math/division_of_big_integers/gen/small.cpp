2a7aa34f4ab96cccb8e43c4460b098d5ddbdae07
NyaanNyaan <suteakadapyon3@gmail.com>
2023-04-11 23:22:20 +0900

    fix constraints and some bugs

--- math/division_of_big_integers/gen/small.cpp
+++ math/division_of_big_integers/gen/small.cpp
@@ -10,21 +10,15 @@ int main() {
   int lsum = 0, flag = true;
   for (int i = 0; flag; i++) {
     for (int j = 1; j <= i * 2 + 10 && flag; j++) {
-      for (int k = 0; k < 2; k++) {
-        for (int l = 0; l < 2; l++) {
-          int a = k ? -i : +i;
-          int b = l ? -j : +j;
-          string sa = to_string(a);
-          string sb = to_string(b);
-          lsum += sa.size() + sb.size();
-          if (lsum > SUM_OF_CHARACTER_LENGTH) {
-            flag = false;
-            break;
-          }
-          A.push_back(sa);
-          B.push_back(sb);
-        }
+      string sa = to_string(i);
+      string sb = to_string(j);
+      lsum += sa.size() + sb.size();
+      if (lsum > SUM_OF_CHARACTER_LENGTH) {
+        flag = false;
+        break;
       }
+      A.push_back(sa);
+      B.push_back(sb);
     }
   }
   printf("%d\n", (int)A.size());

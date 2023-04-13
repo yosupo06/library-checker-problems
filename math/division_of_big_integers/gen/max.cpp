9d7cef21ee377954b6a6a94948b13e1dd6522dc3
NyaanNyaan <suteakadapyon3@gmail.com>
2023-04-13 15:40:17 +0900

    fix and add generator

--- math/division_of_big_integers/gen/max.cpp
+++ math/division_of_big_integers/gen/max.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 
+#include "../base.hpp"
 #include "../params.h"
 using namespace std;
 using ll = long long;
 
-int main(int, char**) {
-  string MAX(LOG_10_A_AND_B_MAX + 1, '0');
-  MAX[0] = '1';
+int main(int, char* argv[]) {
+  long long seed = atoll(argv[1]);
+
+  string max(LOG_10_A_AND_B_MAX + 1, '0');
+  max[0] = '1';
 
   int t = 1;
-  string A = MAX, B = MAX;
+  bigint MAX = max, A, B;
+  switch ((seed % 3 + 3) % 3) {
+    case 0:
+      A = B = MAX;
+      break;
+    case 1:
+      A = MAX, B = 1;
+      break;
+    case 2:
+      A = MAX - 1, B = MAX;
+      break;
+  }
   printf("%d\n", t);
-  printf("%s %s\n", A.c_str(), B.c_str());
+  printf("%s %s\n", A.to_string().c_str(), B.to_string().c_str());
 }

7aea7762bcbd5fe715ef9797b95ad05414ebf4cc
kassy <kassyan323@gmail.com>
2023-04-19 15:36:14 +0900

    len>1

--- datastructure/vertex_add_path_sum/gen/long-path-decomposition_killer.cpp
+++ datastructure/vertex_add_path_sum/gen/long-path-decomposition_killer.cpp
@@ -9,8 +9,8 @@ int main(int, char* argv[]) {
   long long seed = atoll(argv[1]);
   auto gen = Random(seed);
 
-  int sqrtn = (int)sqrt(N_AND_Q_MAX-1);
-  int n = sqrtn*sqrtn+1;
+  int sqrtn = (int)sqrt(N_AND_Q_MAX);
+  int n = sqrtn*sqrtn;
   int q = N_AND_Q_MAX;
   printf("%d %d\n", n, q);
   for (int i = 0; i < n; i++) {
@@ -22,7 +22,7 @@ int main(int, char* argv[]) {
   vector<int> u(n - 1), v(n - 1);
 
   int len=2*sqrtn-1, pr=0, p=0;
-  while (len>0) {
+  while (len>1) {
     assert(p < n - 1);
     u[p]=pr;
     v[p]=pr+len;

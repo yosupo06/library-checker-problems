fd86fc0629df6679320f4a368c7764844958fe47
maspypy <maspypy.gmail.com>
2023-05-03 01:19:08 +0900

    __lg

--- math/polynomial_interpolation_on_geometric_sequence/sol/correct.cpp
+++ math/polynomial_interpolation_on_geometric_sequence/sol/correct.cpp
@@ -252,7 +252,7 @@ vc<mint> middle_product(vc<mint> &a, vc<mint> &b) {
   if constexpr (mint::ntt_info().fi == -1) {
     return middle_product_garner(a, b);
   } else {
-    int n = 1 << __lg(2 * len(a) - 1);
+    int n = 1 << topbit(2 * len(a) - 1);
     vc<mint> fa(n), fb(n);
     copy(a.begin(), a.end(), fa.begin());
     copy(b.rbegin(), b.rend(), fb.begin());

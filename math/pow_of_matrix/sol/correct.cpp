52ccfe7e2c9f0b6a9ca3195ca23e859d4f25ec43
maspypy <maspypy.gmail.com>
2023-11-05 02:33:03 +0900

    reduce template

--- math/pow_of_matrix/sol/correct.cpp
+++ math/pow_of_matrix/sol/correct.cpp
@@ -7,9 +7,7 @@ using namespace std;
 using ll = long long;
 using u32 = unsigned int;
 using u64 = unsigned long long;
-using i128 = __int128;
 using u128 = unsigned __int128;
-using f128 = __float128;
 
 using vi = vector<ll>;
 template <class T>
@@ -41,119 +39,6 @@ using vvc = vector<vc<T>>;
 #define mt make_tuple
 #define fi first
 #define se second
-
-#line 1 "library/other/io.hpp"
-// based on yosupo's fastio
-#include <unistd.h>
-
-#line 2 "library/mod/modint_common.hpp"
-
-struct has_mod_impl {
-  template <class T>
-  static auto check(T &&x) -> decltype(x.get_mod(), std::true_type{});
-  template <class T>
-  static auto check(...) -> std::false_type;
-};
-
-template <class T>
-class has_mod : public decltype(has_mod_impl::check<T>(std::declval<T>())) {};
-
-template <typename mint>
-mint inv(int n) {
-  static const int mod = mint::get_mod();
-  static vector<mint> dat = {0, 1};
-  assert(0 <= n);
-  if (n >= mod) n %= mod;
-  while (len(dat) <= n) {
-    int k = len(dat);
-    int q = (mod + k - 1) / k;
-    dat.eb(dat[k * q - mod] * mint::raw(q));
-  }
-  return dat[n];
-}
-
-template <typename mint>
-mint fact(int n) {
-  static const int mod = mint::get_mod();
-  assert(0 <= n && n < mod);
-  static vector<mint> dat = {1, 1};
-  while (len(dat) <= n) dat.eb(dat[len(dat) - 1] * mint::raw(len(dat)));
-  return dat[n];
-}
-
-template <typename mint>
-mint fact_inv(int n) {
-  static vector<mint> dat = {1, 1};
-  if (n < 0) return mint(0);
-  while (len(dat) <= n) dat.eb(dat[len(dat) - 1] * inv<mint>(len(dat)));
-  return dat[n];
-}
-
-template <class mint, class... Ts>
-mint fact_invs(Ts... xs) {
-  return (mint(1) * ... * fact_inv<mint>(xs));
-}
-
-template <typename mint, class Head, class... Tail>
-mint multinomial(Head &&head, Tail &&... tail) {
-  return fact<mint>(head) * fact_invs<mint>(std::forward<Tail>(tail)...);
-}
-
-template <typename mint>
-mint C_dense(int n, int k) {
-  static vvc<mint> C;
-  static int H = 0, W = 0;
-  auto calc = [&](int i, int j) -> mint {
-    if (i == 0) return (j == 0 ? mint(1) : mint(0));
-    return C[i - 1][j] + (j ? C[i - 1][j - 1] : 0);
-  };
-  if (W <= k) {
-    FOR(i, H) {
-      C[i].resize(k + 1);
-      FOR(j, W, k + 1) { C[i][j] = calc(i, j); }
-    }
-    W = k + 1;
-  }
-  if (H <= n) {
-    C.resize(n + 1);
-    FOR(i, H, n + 1) {
-      C[i].resize(W);
-      FOR(j, W) { C[i][j] = calc(i, j); }
-    }
-    H = n + 1;
-  }
-  return C[n][k];
-}
-
-template <typename mint, bool large = false, bool dense = false>
-mint C(ll n, ll k) {
-  assert(n >= 0);
-  if (k < 0 || n < k) return 0;
-  if constexpr (dense) return C_dense<mint>(n, k);
-  if constexpr (!large) return multinomial<mint>(n, k, n - k);
-  k = min(k, n - k);
-  mint x(1);
-  FOR(i, k) x *= mint(n - i);
-  return x * fact_inv<mint>(k);
-}
-
-template <typename mint, bool large = false>
-mint C_inv(ll n, ll k) {
-  assert(n >= 0);
-  assert(0 <= k && k <= n);
-  if (!large) return fact_inv<mint>(n) * fact<mint>(k) * fact<mint>(n - k);
-  return mint(1) / C<mint, 1>(n, k);
-}
-
-// [x^d](1-x)^{-n}
-template <typename mint, bool large = false, bool dense = false>
-mint C_negative(ll n, ll d) {
-  assert(n >= 0);
-  if (d < 0) return mint(0);
-  if (n == 0) { return (d == 0 ? mint(1) : mint(0)); }
-  return C<mint, large, dense>(n + d - 1, d);
-}
-#line 3 "library/mod/modint.hpp"
 
 template <int mod>
 struct modint {
@@ -213,15 +98,7 @@ struct modint {
     }
     return ret;
   }
-#ifdef FASTIO
-  void write() { fastio::printer.write(val); }
-  void read() {
-    fastio::scanner.read(val);
-    val %= mod;
-  }
-#endif
   static constexpr int get_mod() { return mod; }
-  // (n, r), r は 1 の 2^n 乗根
   static constexpr pair<int, int> ntt_info() {
     if (mod == 167772161) return {25, 17};
     if (mod == 469762049) return {26, 30};
@@ -237,11 +114,9 @@ struct modint {
   static constexpr bool can_ntt() { return ntt_info().fi != -1; }
 };
 
-using modint107 = modint<1000000007>;
 using modint998 = modint<998244353>;
-#line 3 "library/linalg/matrix_mul.hpp"
 
-template <class T, typename enable_if<has_mod<T>::value>::type * = nullptr>
+template <typename T>
 vc<vc<T>> matrix_mul(const vc<vc<T>> &A, const vc<vc<T>> &B, int N1 = -1,
                      int N2 = -1, int N3 = -1) {
   if (N1 == -1) { N1 = len(A), N2 = len(B), N3 = len(B[0]); }
@@ -249,33 +124,13 @@ vc<vc<T>> matrix_mul(const vc<vc<T>> &A, const vc<vc<T>> &B, int N1 = -1,
   FOR(i, N2) FOR(j, N3) b[j][i] = B[i][j].val;
   vv(T, C, N1, N3);
 
-  if ((T::get_mod() < (1 << 30)) && N2 <= 16) {
-    FOR(i, N1) FOR(j, N3) {
-      u64 sm = 0;
-      FOR(m, N2) sm += u64(A[i][m].val) * b[j][m];
-      C[i][j] = sm;
-    }
-  } else {
-    FOR(i, N1) FOR(j, N3) {
-      u128 sm = 0;
-      FOR(m, N2) sm += u64(A[i][m].val) * b[j][m];
-      C[i][j] = T::raw(sm % (T::get_mod()));
-    }
+  FOR(i, N1) FOR(j, N3) {
+    u128 sm = 0;
+    FOR(m, N2) sm += u64(A[i][m].val) * b[j][m];
+    C[i][j] = T::raw(sm % (T::get_mod()));
   }
   return C;
 }
-
-template <class T, typename enable_if<!has_mod<T>::value>::type * = nullptr>
-vc<vc<T>> matrix_mul(const vc<vc<T>> &A, const vc<vc<T>> &B, int N1 = -1,
-                     int N2 = -1, int N3 = -1) {
-  if (N1 == -1) { N1 = len(A), N2 = len(B), N3 = len(B[0]); }
-  vv(T, b, N2, N3);
-  FOR(i, N2) FOR(j, N3) b[j][i] = B[i][j];
-  vv(T, C, N1, N3);
-  FOR(n, N1) FOR(m, N2) FOR(k, N3) C[n][k] += A[n][m] * b[k][m];
-  return C;
-}
-#line 2 "library/linalg/matrix_pow.hpp"
 
 template <typename T>
 vc<vc<T>> matrix_pow(vc<vc<T>> A, ll n) {
@@ -289,7 +144,7 @@ vc<vc<T>> matrix_pow(vc<vc<T>> A, ll n) {
   }
   return ret;
 }
-#line 6 "main.cpp"
+
 using mint = modint998;
 
 void solve() {

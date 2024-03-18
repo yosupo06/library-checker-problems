87846d91f17b4623ade9c1f0ef0d4927d078fa53
maspypy <45988013+maspypy@users.noreply.github.com>
2024-03-19 05:33:30 +0900

    Update correct.cpp

--- math/compositional_inverse_of_formal_power_series_large/sol/correct.cpp
+++ math/compositional_inverse_of_formal_power_series_large/sol/correct.cpp
@@ -426,34 +426,6 @@ vc<mint> poly_taylor_shift(vc<mint> f, mint c) {
   return f;
 }
 
-// n, m 次多項式 (n>=m) a, b → n-m 次多項式 c
-// c[i] = sum_j b[j]a[i+j]
-template <typename mint>
-vc<mint> middle_product(vc<mint>& a, vc<mint>& b) {
-  assert(len(a) >= len(b));
-  if (b.empty()) return vc<mint>(len(a) - len(b) + 1);
-  if (min(len(b), len(a) - len(b) + 1) <= 60) {
-    return middle_product_naive(a, b);
-  }
-  int n = 1 << __lg(2 * len(a) - 1);
-  vc<mint> fa(n), fb(n);
-  copy(a.begin(), a.end(), fa.begin());
-  copy(b.rbegin(), b.rend(), fb.begin());
-  ntt(fa, 0), ntt(fb, 0);
-  FOR(i, n) fa[i] *= fb[i];
-  ntt(fa, 1);
-  fa.resize(len(a));
-  fa.erase(fa.begin(), fa.begin() + len(b) - 1);
-  return fa;
-}
-
-template <typename mint>
-vc<mint> middle_product_naive(vc<mint>& a, vc<mint>& b) {
-  vc<mint> res(len(a) - len(b) + 1);
-  FOR(i, len(res)) FOR(j, len(b)) res[i] += b[j] * a[i + j];
-  return res;
-}
-
 template <typename mint>
 int count_terms(const vc<mint>& f) {
   int t = 0;

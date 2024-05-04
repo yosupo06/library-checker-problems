7972f608817a006a0c424b8bcab866a18dce1e92
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2024-05-04 14:48:51 +0900

    write: random_ab

---
+++ string/wildcard_pattern_matching/gen/random_ab.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include "random.h"
+
+int main(int, char *argv[])
+{
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    int n = gen.uniform(1, (1 << 19) - 1);
+    int m = gen.uniform(1, n);
+
+    auto gen_asterisk_string_ab = [&gen](size_t n)
+    {
+        std::string s(n, '*');
+        for (char &c : s)
+            if (gen.uniform(1, 100) == 1)
+                c = gen.uniform('a', 'b');
+        return s;
+    };
+
+    std::string s = gen_asterisk_string_ab(n);
+    std::string t = gen_asterisk_string_ab(m);
+
+    printf("%s\n", s.c_str());
+    printf("%s\n", t.c_str());
+    return 0;
+}

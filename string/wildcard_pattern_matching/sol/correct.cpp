b14403a5e92322a49dd1701bc25a50782cd9982f
dr0gsk0l <sohta0907.kamenrider@gmail.com>
2024-05-04 13:36:27 +0900

    fix: correct

--- string/wildcard_pattern_matching/sol/correct.cpp
+++ string/wildcard_pattern_matching/sol/correct.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 #include <tuple>
 
-#include <atcoder/all>
+#include <atcoder/convolution>
 
 using ll = long long;
 
@@ -16,15 +16,15 @@ std::string scan()
 auto make_vectors(const std::string &s)
 {
     size_t n = s.size();
-    std::vector<ll> s1(n), s2(n), s3(n);
+    std::vector v(3, std::vector<ll>(n));
     for (size_t i = 0; i < n; i++)
     {
         ll a = s[i] == '*' ? 0 : s[i] - 'a' + 1;
-        s1[i] = a;
-        s2[i] = a * a;
-        s3[i] = a * a * a;
+        v[0][i] = a;
+        v[1][i] = a * a;
+        v[2][i] = a * a * a;
     }
-    return std::make_tuple(s1, s2, s3);
+    return v;
 }
 
 int main()
@@ -32,4 +32,22 @@ int main()
 
     std::string s = scan();
     std::string t = scan();
+
+    std::reverse(t.begin(), t.end());
+
+    auto S = make_vectors(s);
+    auto T = make_vectors(t);
+
+    size_t n = s.size(), m = t.size();
+    std::vector<ll> sum(n - m + 1, 0);
+    for (size_t j = 0; j < 3; j++)
+    {
+        auto st = atcoder::convolution_ll(S[j], T[2 - j]);
+        for (size_t i = 0; i <= n - m; i++)
+            sum[i] += st[i];
+    }
+
+    for (auto a : sum)
+        std::cout << bool(a);
+    std::cout << "\n";
 }

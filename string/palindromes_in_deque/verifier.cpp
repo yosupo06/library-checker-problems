88a800320a06e1a270518b626944fb55c0e38e28
Nachia Vivias <piyopiyograph@gol.com>
2024-05-27 03:08:07 +0900

    [string/palindromes_in_deque] fix according to review comment
    https://github.com/yosupo06/library-checker-problems/pull/1157#discussion_r1615255399
    https://github.com/yosupo06/library-checker-problems/pull/1157#discussion_r1615255716

--- string/palindromes_in_deque/verifier.cpp
+++ string/palindromes_in_deque/verifier.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
     registerValidation();
 
-    int q = inf.readInt(1, Q_MAX, "Q");
+    int q = inf.readInt(Q_MIN, Q_MAX, "Q");
     inf.readChar('\n');
 
     int length = 0;
@@ -13,7 +13,8 @@ int main() {
         int t = inf.readInt(0, 3, "t");
         if (t == 0 || t == 1) {
             inf.readSpace();
-            inf.readChar();
+            char ch = inf.readChar();
+            ensure('a' <= ch && ch <= 'z');
             length++;
         } else if (t == 2 || t == 3) {
             length--;

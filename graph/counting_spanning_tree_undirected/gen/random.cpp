61e633873043f08cf8b6039af4b59b5882458f4f
maspypy <45988013+maspypy@users.noreply.github.com>
2024-06-02 19:58:10 +0900

    Update graph/counting_spanning_tree_undirected/gen/random.cpp
    Co-authored-by: NachiaVivias <71425187+NachiaVivias@users.noreply.github.com>

--- graph/counting_spanning_tree_undirected/gen/random.cpp
+++ graph/counting_spanning_tree_undirected/gen/random.cpp
@@ -5,8 +5,8 @@ int main(int, char* argv[]) {
   long long seed = atoll(argv[1]);
   auto gen = Random(seed);
 
-  int N = gen.uniform<int>(1, N_MAX);
-  int M = gen.uniform<int>(0, M_MAX);
+  int N = gen.uniform<int>(N_MIN, N_MAX);
+  int M = gen.uniform<int>(M_MIN, M_MAX);
   printf("%d %d\n", N, M);
   for (int i = 0; i < M; ++i) {
     int a = gen.uniform<int>(0, N - 1);

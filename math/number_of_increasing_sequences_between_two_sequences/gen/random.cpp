cc2a05707f1e55170f2d774e0ed102f010d8001b
Tomohisa Hachiya <tomo@TomohisanoMacBook-Air.local>
2024-06-10 14:35:41 +0900

    一通り完成

---
+++ math/number_of_increasing_sequences_between_two_sequences/gen/random.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+#include "random.h"
+#include "../params.h"
+using namespace std;
+
+int main(int, char* argv[]) {
+
+    long long seed = atoll(argv[1]);
+    auto gen = Random(seed);
+
+    int n = gen.uniform<int>(1, N_M_MAX);
+    int m = gen.uniform<int>(1, N_M_MAX);
+    
+    printf("%d %d\n", n, m);
+
+    vector<int> a(n), b(n);
+    for(int i = 0; i < n; i++){
+        a[i] = gen.uniform<int>(0, m - 1);
+        b[i] = gen.uniform<int>(a[i] + 1, m);
+        if(a[i] == b[i]){
+            if(a[i] != 0){
+                a[i]--;
+            }
+            else{
+                b[i]++;
+            }
+        }
+        if(a[i] > b[i]){
+            swap(a[i], b[i]);
+        }
+    }
+
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+
+    for(int i = 0; i < n; i++){
+        a[i] += gen.uniform<int>(-2, 2);
+        if(a[i] < 0)a[i] = 0;
+        if(a[i] > m)a[i] = m;
+        b[i] += gen.uniform<int>(-2, 2);
+        if(b[i] < 0)b[i] = 0;
+        if(b[i] > m)b[i] = m;
+        if(a[i] == b[i]){
+            if(a[i] != 0){
+                a[i]--;
+            }
+            else{
+                b[i]++;
+            }
+        }
+        if(a[i] > b[i]){
+            swap(a[i], b[i]);
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        printf("%d", a[i]);
+        if(i == n - 1){
+            printf("\n");
+        }
+        else{
+            printf(" ");
+        }
+    }
+
+    for(int i = 0; i < n; i++){
+        printf("%d", b[i]);
+        if(i == n - 1){
+            printf("\n");
+        }
+        else{
+            printf(" ");
+        }
+    }
+
+    return 0;
+}
---
+++ math/number_of_increasing_sequences_between_two_sequences/verifier.cpp
@@ -0,0 +1,35 @@
+#include "testlib.h"
+#include "params.h"
+
+#include <vector>
+
+using namespace std;
+
+int main() {
+    registerValidation();
+
+    int n = inf.readInt(1, N_M_MAX); // a
+    inf.readSpace();
+    int m = inf.readInt(1, N_M_MAX); // b
+    inf.readChar('\n');
+    vector<int> a(n), b(n);
+    for(int i = 0; i < n; i++){
+        a[i] = inf.readInt(0, m);
+        if(i < n - 1){
+            inf.readSpace();
+        }
+    }
+    inf.readChar('\n');
+    for(int i = 0; i < n; i++){
+        b[i] = inf.readInt(0, m);
+        if(i < n - 1){
+            inf.readSpace();
+        }
+    }
+    inf.readChar('\n');
+    for(int i = 0; i < n; i++){
+        assert(a[i] < b[i]);
+    }
+    inf.readEof();
+    return 0;
+}

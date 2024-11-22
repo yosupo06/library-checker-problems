4007c7f258a17a4351a5e23860f198ec43c19ed3
Oleksandr Kulkov <adamant.pwn@gmail.com>
2024-11-22 18:08:39 +0100

    Update

--- convolution/convolution_mod_large/gen/fft_killer.cpp
+++ convolution/convolution_mod_large/gen/fft_killer.cpp
@@ -9,13 +9,13 @@ using namespace std;
 int main(int, char* argv[]) {
   long long seed = atoll(argv[1]);
   auto gen = Random(seed);
-  auto sub = (seed & 1) ? 1000 : 10;
-  auto div = (seed & 2) ? 1 : 2;
+  auto sub = (seed <= 1) || ((seed - 2) & 1) ? 1000 : 10;
+  auto div = (seed <= 1) || ((seed - 2) & 2) ? 1 : 2;
 
   int n = N_AND_M_MAX;
   int m = N_AND_M_MAX;
   vector<int> a(n), b(m);
-    const int B = (seed & 4) ? 1<<15 : std::sqrt(MOD);
+    const int B = (seed <= 1) || ((seed - 2) & 4) ? 1<<15 : std::sqrt(MOD);
   for (int i = 0; i < n; i++) {
     int lw = gen.uniform(B - sub, B - 1);
     int up = gen.uniform(MOD / div / B - sub, MOD / div / B - 1);
--- polynomial/polynomial_taylor_shift/gen/fft_killer.cpp
+++ polynomial/polynomial_taylor_shift/gen/fft_killer.cpp
@@ -8,12 +8,12 @@ int main(int, char *argv[])
 {
     long long seed = atoll(argv[1]);
     auto gen = Random(seed);
-    auto sub = (seed & 1) ? 1000 : 10;
-    auto div = (seed & 2) ? 1 : 2;
+    auto sub = (seed <= 1) || ((seed - 2) & 1) ? 1000 : 10;
+    auto div = (seed <= 1) || ((seed - 2) & 2) ? 1 : 2;
 
     int n = N_MAX;
     vector<int> a(n);
-    const int B = (seed & 4) ? 1<<15 : std::sqrt(MOD);
+    const int B = (seed <= 1) || ((seed - 2) & 4) ? 1<<15 : std::sqrt(MOD);
     for (int i = 0; i < n; i++)
     {
         int lw = gen.uniform(B - sub, B - 1);

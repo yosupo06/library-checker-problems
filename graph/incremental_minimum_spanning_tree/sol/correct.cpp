2f1a0212965e734d36c8ca5fbc58963dbfd92eb4
xdbwk <xindubawukong@gmail.com>
2025-02-28 17:13:09 -0800

    fix

--- graph/incremental_minimum_spanning_tree/sol/correct.cpp
+++ graph/incremental_minimum_spanning_tree/sol/correct.cpp
@@ -1,85 +1,96 @@
 #include <cassert>
 #include <cstdio>
+#include <limits>
 #include <map>
-#include <numeric>
 #include <vector>
 using namespace std;
 
-template <typename Info>
-struct SplayTree {
+template <typename Info> struct SplayTree {
   using info_t = Info;
   struct Node : public Info {
     Node *lch, *rch, *fa;
     Node(Info info) : Info(info), lch(nullptr), rch(nullptr), fa(nullptr){};
   };
-  Node* root;
+  Node *root;
   SplayTree() : root(nullptr) {}
-  Node* Update(Node* x) {
+  Node *Update(Node *x) {
     x->Update();
     return x;
   }
-  void PushDown(Node* x) {
+  void PushDown(Node *x) {
     if (x->NeedPushDown()) {
       x->PushDown();
     }
   }
 
-  void Zig(Node* t) {
+  void Zig(Node *t) {
     auto y = t->fa;
     PushDown(y), PushDown(t);
     t->fa = y->fa;
     if (y->fa) {
-      if (y == y->fa->lch) y->fa->lch = t;
-      else y->fa->rch = t;
+      if (y == y->fa->lch)
+        y->fa->lch = t;
+      else
+        y->fa->rch = t;
     }
     y->lch = t->rch;
-    if (t->rch) t->rch->fa = y;
+    if (t->rch)
+      t->rch->fa = y;
     t->rch = y;
     y->fa = t;
     Update(y), Update(t);
   }
 
-  void Zag(Node* t) {
+  void Zag(Node *t) {
     auto y = t->fa;
     PushDown(y), PushDown(t);
     t->fa = y->fa;
     if (y->fa) {
-      if (y == y->fa->lch) y->fa->lch = t;
-      else y->fa->rch = t;
+      if (y == y->fa->lch)
+        y->fa->lch = t;
+      else
+        y->fa->rch = t;
     }
     y->rch = t->lch;
-    if (t->lch) t->lch->fa = y;
+    if (t->lch)
+      t->lch->fa = y;
     t->lch = y;
     y->fa = t;
     Update(y), Update(t);
   }
 
-  void Splay(Node* t, Node* p = nullptr) {
+  void Splay(Node *t, Node *p = nullptr) {
     assert(t);
     while (t->fa != p) {
       auto y = t->fa;
       if (y->fa == p) {
-        if (t == y->lch) Zig(t);
-        else Zag(t);
+        if (t == y->lch)
+          Zig(t);
+        else
+          Zag(t);
       } else {
         if (y == y->fa->lch) {
-          if (t == y->lch) Zig(y), Zig(t);
-          else Zag(t), Zig(t);
+          if (t == y->lch)
+            Zig(y), Zig(t);
+          else
+            Zag(t), Zig(t);
         } else {
-          if (t == y->rch) Zag(y), Zag(t);
-          else Zig(t), Zag(t);
+          if (t == y->rch)
+            Zag(y), Zag(t);
+          else
+            Zig(t), Zag(t);
         }
       }
     }
-    if (!p) root = t;
+    if (!p)
+      root = t;
   }
 };
 
-template <typename Info>
-struct LinkCutTree {
+template <typename Info> struct LinkCutTree {
   struct SplayInfo : public Info {
-    typename SplayTree<SplayInfo>::Node* Node() {
-      return reinterpret_cast<typename SplayTree<SplayInfo>::Node*>(this);
+    typename SplayTree<SplayInfo>::Node *Node() {
+      return reinterpret_cast<typename SplayTree<SplayInfo>::Node *>(this);
     }
     bool rev;
     int id, upid;
@@ -94,28 +105,32 @@ struct LinkCutTree {
     void PushDown() {
       auto lch = Node()->lch, rch = Node()->rch;
       if (rev) {
-        if (lch) lch->Reverse();
-        if (rch) rch->Reverse();
+        if (lch)
+          lch->Reverse();
+        if (rch)
+          rch->Reverse();
         rev = false;
       }
       if (Info::NeedPushDown()) {
-        Info::PushDown(lch ? (Info*)lch : nullptr, rch ? (Info*)rch : nullptr);
+        Info::PushDown(lch ? (Info *)lch : nullptr,
+                       rch ? (Info *)rch : nullptr);
       }
     }
     void Update() {
       auto lch = Node()->lch, rch = Node()->rch;
-      Info::Update(lch ? (Info*)lch : nullptr, rch ? (Info*)rch : nullptr);
+      Info::Update(lch ? (Info *)lch : nullptr, rch ? (Info *)rch : nullptr);
     }
   };
 
   using Node = typename SplayTree<SplayInfo>::Node;
 
   int n;
-  std::vector<Node*> node;
+  std::vector<Node *> node;
   SplayTree<SplayInfo> splay;
   LinkCutTree(int n_) : n(0) { ExpandTo(n_); }
   void ExpandTo(int m) {
-    if (m <= n) return;
+    if (m <= n)
+      return;
     node.resize(m);
     for (int i = n; i < m; i++) {
       SplayInfo info;
@@ -124,27 +139,30 @@ struct LinkCutTree {
     }
     n = m;
   }
-  void Splay(Node* x) {
+  void Splay(Node *x) {
     auto y = x;
-    while (y->fa) y = y->fa;
+    while (y->fa)
+      y = y->fa;
     splay.Splay(x);
     std::swap(x->upid, y->upid);
     x->Update();
   }
   void Splay(int x) { Splay(node[x]); }
-  void Access(Node* x) {
-    for (Node* y = nullptr; x;
+  void Access(Node *x) {
+    for (Node *y = nullptr; x;
          y = x, x = x->upid == -1 ? nullptr : node[x->upid]) {
       Splay(x);
       splay.PushDown(x);
-      if (x->rch) x->rch->fa = nullptr, x->rch->upid = x->id;
+      if (x->rch)
+        x->rch->fa = nullptr, x->rch->upid = x->id;
       x->rch = y;
-      if (y) y->fa = x, y->upid = -1;
+      if (y)
+        y->fa = x, y->upid = -1;
       splay.Update(x);
     }
   }
   void Access(int x) { Access(node[x]); }
-  Node* GetRoot(Node* x) {
+  Node *GetRoot(Node *x) {
     Access(x);
     Splay(x);
     while (x->lch) {
@@ -155,18 +173,18 @@ struct LinkCutTree {
     return x;
   }
   int GetRoot(int x) { return GetRoot(node[x])->id; }
-  void MakeRoot(Node* x) {
+  void MakeRoot(Node *x) {
     Access(x);
     Splay(x);
     x->Reverse();
   }
   void MakeRoot(int x) { MakeRoot(node[x]); }
-  void Link(Node* x, Node* y) {
+  void Link(Node *x, Node *y) {
     MakeRoot(x);
     x->upid = y->id;
   }
   void Link(int x, int y) { Link(node[x], node[y]); }
-  void Cut(Node* x, Node* y) {
+  void Cut(Node *x, Node *y) {
     MakeRoot(x);
     Access(y);
     Splay(y);
@@ -181,16 +199,17 @@ struct Info {
   int val, edge_id;
   pair<int, int> max_val;
   Info()
-      : val(std::numeric_limits<int>::min()),
-        edge_id(-1),
+      : val(std::numeric_limits<int>::min()), edge_id(-1),
         max_val({std::numeric_limits<int>::min(), -1}) {}
   void Reverse() {}
   bool NeedPushDown() { return false; }
-  void PushDown(Info*, Info*) {}
-  void Update(Info* a, Info* b) {
+  void PushDown(Info *, Info *) {}
+  void Update(Info *a, Info *b) {
     max_val = {val, edge_id};
-    if (a) max_val = max(max_val, a->max_val);
-    if (b) max_val = max(max_val, b->max_val);
+    if (a)
+      max_val = max(max_val, a->max_val);
+    if (b)
+      max_val = max(max_val, b->max_val);
   }
 };
 

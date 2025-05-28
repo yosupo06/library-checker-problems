1d8e8ce8ff45df48347256cb7136b9808be505b1
37kt <jinmatakich84@gmail.com>
2025-05-28 06:16:55 +0000

    add verifier.cpp

---
+++ data_structure/majority_voting/verifier.cpp
@@ -0,0 +1,39 @@
+#include "params.h"
+#include "testlib.h"
+#include <vector>
+
+using namespace std;
+
+int main() {
+  registerValidation();
+
+  int N = inf.readInt(N_MIN, N_MAX);
+  inf.readSpace();
+  int Q = inf.readInt(Q_MIN, Q_MAX);
+  inf.readChar('\n');
+
+  vector<int> A(N);
+  for (int i = 0; i < N; i++) {
+    if (i) inf.readSpace();
+    A[i] = inf.readInt(A_MIN, A_MAX);
+  }
+  inf.readChar('\n');
+
+  for (int i = 0; i < Q; i++) {
+    int t = inf.readInt(0, 1);
+    inf.readSpace();
+
+    if (t == 0) {
+      int p = inf.readInt(0, N - 1);
+      inf.readSpace();
+      int x = inf.readInt(A_MIN, A_MAX);
+    } else {
+      int l = inf.readInt(0, N - 1);
+      inf.readSpace();
+      int r = inf.readInt(l + 1, N);
+    }
+    inf.readChar('\n');
+  }
+  inf.readEof();
+  return 0;
+}

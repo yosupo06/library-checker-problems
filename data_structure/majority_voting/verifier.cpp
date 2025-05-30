f945d846d1fdbcec7031d2ebd8a94903d9d0a3b5
37kt <jinmatakich84@gmail.com>
2025-05-30 02:21:27 +0000

    format

--- data_structure/majority_voting/verifier.cpp
+++ data_structure/majority_voting/verifier.cpp
@@ -5,36 +5,36 @@
 using namespace std;
 
 int main() {
-  registerValidation();
+    registerValidation();
 
-  int N = inf.readInt(N_MIN, N_MAX);
-  inf.readSpace();
-  int Q = inf.readInt(Q_MIN, Q_MAX);
-  inf.readChar('\n');
-
-  vector<int> A(N);
-  for (int i = 0; i < N; i++) {
-    if (i)
-      inf.readSpace();
-    A[i] = inf.readInt(A_MIN, A_MAX);
-  }
-  inf.readChar('\n');
-
-  for (int i = 0; i < Q; i++) {
-    int t = inf.readInt(0, 1);
+    int N = inf.readInt(N_MIN, N_MAX);
     inf.readSpace();
+    int Q = inf.readInt(Q_MIN, Q_MAX);
+    inf.readChar('\n');
 
-    if (t == 0) {
-      inf.readInt(0, N - 1);
-      inf.readSpace();
-      inf.readInt(A_MIN, A_MAX);
-    } else {
-      int l = inf.readInt(0, N - 1);
-      inf.readSpace();
-      inf.readInt(l + 1, N);
+    vector<int> A(N);
+    for (int i = 0; i < N; i++) {
+        if (i)
+            inf.readSpace();
+        A[i] = inf.readInt(A_MIN, A_MAX);
     }
     inf.readChar('\n');
-  }
-  inf.readEof();
-  return 0;
+
+    for (int i = 0; i < Q; i++) {
+        int t = inf.readInt(0, 1);
+        inf.readSpace();
+
+        if (t == 0) {
+            inf.readInt(0, N - 1);
+            inf.readSpace();
+            inf.readInt(A_MIN, A_MAX);
+        } else {
+            int l = inf.readInt(0, N - 1);
+            inf.readSpace();
+            inf.readInt(l + 1, N);
+        }
+        inf.readChar('\n');
+    }
+    inf.readEof();
+    return 0;
 }

9a867f57edeb69b9515c00e18f3748194f1970b2
Nicholas Patrick <45913457+NicholasPatrick@users.noreply.github.com>
2026-02-02 11:25:37 +0700

    Unremove old test and add new test.

--- linear_algebra/matrix_product/gen/unsigned_overflow.cpp
+++ linear_algebra/matrix_product/gen/unsigned_overflow.cpp
@@ -7,13 +7,26 @@
 
 using namespace std;
 
-int main() {
+int main(int, char* argv[]) {
+    long long seed = atoll(argv[1]);
+
     int val = MOD - 2;
     unsigned long long max_ull = numeric_limits<unsigned long long>::max();
 
     // Find smallest size n where n * val * val > max_ull
+    int n = 0;
     unsigned long long val2 = (unsigned long long) val * val;
-    int n = max_ull / val2 + 1;
+    if (seed == 0) {
+        unsigned long long res = 0;
+        // Check if adding val2 to res will overflow
+        // this test mistakenly produces the largest n that does not overflow
+        while (res + val2 > res) {
+            res += val2;
+            ++n;
+        }
+    } else if (seed == 1) {
+        n = max_ull / val2 + 1;
+    }
 
     cout << n << ' ' << n << ' ' << n << '\n';
    
